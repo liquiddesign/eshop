bacdb90eb5ed74f86c07d6037592d36418e4257a
Petr Křehlík <petr@lqd.cz>
2021-03-09 19:38:42 +0100

    rekapitulace

--- src/Controls/ICartRecapitulationListFactory.php
+++ src/Controls/ICartRecapitulationListFactory.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Eshop\Controls;
 
+use StORM\ICollection;
+
 interface ICartRecapitulationListFactory
 {
-	public function create(): CartRecapitulationList;
+	public function create(?ICollection $items = null): CartRecapitulationList;
 }
