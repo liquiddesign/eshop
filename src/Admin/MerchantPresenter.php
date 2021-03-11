52f9ed760306122ef272c7caeecd85b366344db6
Oldrich <oldrich@lqd.cz>
2021-03-11 10:10:07 +0100

    eshop init

--- src/Admin/MerchantPresenter.php
+++ src/Admin/MerchantPresenter.php
@@ -22,95 +22,100 @@ use Security\DB\AccountRepository;
 class MerchantPresenter extends Presenter
 {
 	use PresenterTrait;
-
+	
 	/** @inject */
 	public AccountFormFactory $accountFormFactory;
-
+	
 	/** @inject */
 	public MerchantRepository $merchantRepository;
-
+	
 	/** @inject */
 	public AccountRepository $accountRepository;
-
+	
 	/** @inject */
 	public TemplateRepository $templateRepository;
-
+	
 	/** @inject */
 	public Mailer $mailer;
-
+	
 	public function createComponentGrid()
 	{
 		$grid = $this->gridFactory->create($this->merchantRepository->many(), 20, 'code', 'ASC', true);
 		$grid->addColumnSelector();
-
+		
 		$grid->addColumnText('Kód', 'code', '%s', 'code', ['class' => 'fit']);
 		$grid->addColumnText('Jméno a příjmení', 'fullname', '%s', 'fullname');
 		$grid->addColumnText('Email', 'email', '<a href="mailto:%1$s"><i class="far fa-envelope"></i> %1$s</a>')->onRenderCell[] = [$grid, 'decoratorEmpty'];
-
+		
 		$btnSecondary = 'btn btn-sm btn-outline-primary';
 		$grid->addColumn('', function (Merchant $object, Datagrid $datagrid) use ($btnSecondary) {
 			return $object->getAccount() != null ?
 				"<a class='$btnSecondary' href='" . $datagrid->getPresenter()->link('editAccount', $object) . "'>Detail&nbsp;účtu</a>" :
 				"<a class='$btnSecondary' href='" . $datagrid->getPresenter()->link('newAccount', $object) . "'>Vytvořit&nbsp;účet</a>";
 		}, '%s', null, ['class' => 'minimal']);
-
+		
 		$grid->addColumn('Login', function (Merchant $object, Datagrid $grid) use ($btnSecondary) {
 			$link = $object->getAccount() ? $grid->getPresenter()->link('loginMerchant!', [$object->getAccount()->login]) : '#';
-
+			
 			return "<a class='" . ($object->getAccount() ? '' : 'disabled') . " $btnSecondary' target='_blank' href='$link'><i class='fa fa-sign-in-alt'></i></a>";
 		}, '%s', null, ['class' => 'minimal']);
-
+		
 		$grid->addColumnLinkDetail('Detail');
 		$grid->addColumnActionDelete([$this->accountFormFactory, 'deleteAccountHolder']);
-
+		
 		$grid->addButtonDeleteSelected([$this->accountFormFactory, 'deleteAccountHolder']);
-
+		
 		$grid->addFilterTextInput('search', ['code', 'fullName', 'email'], null, 'Jméno, kód, email');
 		$grid->addFilterButtons();
-
+		
 		return $grid;
 	}
-
+	
 	public function createComponentForm(): Form
 	{
 		$form = $this->formFactory->create();
-
+		
 		$form->addText('code', 'Kód');
 		$form->addText('fullname', 'Jméno a příjmení')->setRequired();
 		$form->addEmail('email', 'Email')->setRequired();
-
+		
 		if (!$this->getParameter('merchant')) {
 			$this->accountFormFactory->addContainer($form);
 		}
-
+		
 		$form->addSubmits(!$this->getParameter('merchant'));
-
+		
 		$form->onSuccess[] = function (AdminForm $form) {
 			$values = $form->getValues('array');
-			$form['account']['email']->setValue($values['email']);
-
-			unset($values['account']);
-
+			
+			if (isset($form['account'])) {
+				$form['account']['email']->setValue($values['email']);
+				unset($values['account']);
+			}
+			
 			$merchant = $this->merchantRepository->syncOne($values, null, true);
-			$this->accountFormFactory->onCreateAccount[] = function ($account) use ($merchant) {
-				$merchant->update(['account' => $account]);
-			};
-			$this->accountFormFactory->success($form,'merchant.register.successAdmin');
-
+			
+			if (isset($form['account'])) {
+				$this->accountFormFactory->onCreateAccount[] = function ($account) use ($merchant) {
+					$merchant->update(['account' => $account]);
+				};
+				$this->accountFormFactory->success($form, 'merchant.register.successAdmin');
+			}
+			
 			$this->flashMessage('Uloženo', 'success');
 			$form->processRedirect('detail', 'default', [$merchant]);
 		};
-
+		
 		return $form;
 	}
-
+	
 	public function handleLoginMerchant(string $login)
 	{
 		$this->user->login($login, '', [Merchant::class], true);
-
+		
 		$this->presenter->redirect(':Web:Index:default');
 	}
-
+	
 	public function renderDefault()
 	{
 		$this->template->headerLabel = 'Obchodníci';
@@ -127,7 +132,7 @@ class MerchantPresenter extends Presenter
 		$form['account']['password']->setRequired();
 		$form['account']['passwordCheck']->setRequired();
 	}
-
+	
 	public function renderNew()
 	{
 		$this->template->headerLabel = 'Nová položka';
@@ -138,7 +143,7 @@ class MerchantPresenter extends Presenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('form')];
 	}
-
+	
 	public function renderDetail(Merchant $merchant)
 	{
 		$this->template->headerLabel = 'Detail obchodníka - ' . $merchant->fullname;
@@ -149,33 +154,33 @@ class MerchantPresenter extends Presenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('form')];
 	}
-
+	
 	public function actionDetail(Merchant $merchant)
 	{
 		/** @var Form $form */
 		$form = $this->getComponent('form');
-
+		
 		$form->setDefaults($merchant->toArray());
 	}
-
+	
 	public function createComponentAccountForm()
 	{
 		return $this->accountFormFactory->create();
 	}
-
+	
 	public function actionEditAccount(Merchant $merchant)
 	{
 		/** @var Form $form */
 		$form = $this->getComponent('accountForm');
 		$form['account']['email']->setDefaultValue($merchant->email);
 		$form['account']->setDefaults($merchant->account->toArray());
-
+		
 		$this->accountFormFactory->onDeleteAccount[] = function () {
 			$this->flashMessage('Účet byl smazán', 'success');
 			$this->redirect('default');
 		};
 	}
-
+	
 	public function renderEditAccount(Merchant $merchant): void
 	{
 		$this->template->headerLabel = 'Detail účtu - ' . $merchant->fullname;
@@ -186,19 +191,19 @@ class MerchantPresenter extends Presenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('accountForm')];
 	}
-
+	
 	public function actionNewAccount(Merchant $merchant)
 	{
 		$form = $this->getComponent('accountForm');
 		$form['account']['password']->setRequired();
 		$form['account']['passwordCheck']->setRequired();
 		unset($form['delete']);
-
+		
 		$this->accountFormFactory->onCreateAccount[] = function (Account $account) use ($merchant) {
 			$merchant->update(['account' => $account]);
 		};
 	}
-
+	
 	public function renderNewAccount(Merchant $merchant): void
 	{
 		$this->template->headerLabel = 'Nový účet';
