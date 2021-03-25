803a55812d4d6092a8e498430f2acbba7739516b
Petr Křehlík <petr@lqd.cz>
2021-03-25 16:54:09 +0100

    mailerlite

--- src/Admin/CustomerPresenter.php
+++ src/Admin/CustomerPresenter.php
@@ -16,6 +16,7 @@ use Eshop\DB\Customer;
 use Eshop\DB\CustomerGroupRepository;
 use Eshop\DB\CustomerRepository;
 use Eshop\DB\MerchantRepository;
+use Eshop\Integration\MailerLite;
 use Forms\Form;
 use Grid\Datagrid;
 use League\Csv\Writer;
@@ -31,43 +32,46 @@ class CustomerPresenter extends BackendPresenter
 {
 	/** @inject */
 	public AccountFormFactory $accountFormFactory;
-	
+
 	/** @inject */
 	public CustomerRepository $customerRepository;
-	
+
 	/** @inject */
 	public AccountRepository $accountRepository;
-	
+
 	/** @inject */
 	public MerchantRepository $merchantRepository;
-	
+
 	/** @inject */
 	public TemplateRepository $templateRepository;
-	
+
 	/** @inject */
 	public ProductRepository $productRepo;
-	
+
 	/** @inject */
 	public PaymentTypeRepository $paymentTypeRepo;
-	
+
 	/** @inject */
 	public DeliveryTypeRepository $deliveryTypeRepo;
-	
+
 	/** @inject */
 	public CurrencyRepository $currencyRepo;
-	
+
 	/** @inject */
 	public CustomerGroupRepository $groupsRepo;
-	
+
 	/** @inject */
 	public AddressRepository $addressRepo;
-	
+
 	/** @inject */
 	public Mailer $mailer;
-	
+
 	/** @inject */
 	public PricelistRepository $pricelistRepo;
-	
+
+	/** @inject */
+	public MailerLite $mailerLite;
+
 	public function createComponentCustomers()
 	{
 		$grid = $this->gridFactory->create($this->customerRepository->many(), 20, 'createdTs', 'DESC', true);
@@ -79,106 +83,106 @@ class CustomerPresenter extends BackendPresenter
 		$grid->addColumnText('Skupina', 'group.name', '%s', 'group.name');
 		$grid->addColumnText('Telefon', 'phone', '<a href="tel:%1$s"><i class="fa fa-phone-alt"></i> %1$s</a>')->onRenderCell[] = [$grid, 'decoratorEmpty'];
 		$grid->addColumnText('Email', 'email', '<a href="mailto:%1$s"><i class="far fa-envelope"></i> %1$s</a>')->onRenderCell[] = [$grid, 'decoratorEmpty'];
-		
+
 		$btnSecondary = 'btn btn-sm btn-outline-primary';
 		$grid->addColumn('Feed', function (Customer $customer) use ($btnSecondary) {
 			return "<a class='$btnSecondary' target='_blank' href='" . $this->link('//:Eshop:Export:supplier', $customer->getPK()) . "'><i class='fa fa-sm fa-rss'></i></a>";
 		}, '%s', null, ['class' => 'minimal']);
-		
+
 		$grid->addColumn('Login', function (Customer $object, Datagrid $grid) use ($btnSecondary) {
 			$link = $object->getAccount() ? $grid->getPresenter()->link('loginCustomer!', [$object->getAccount()->login]) : '#';
-			
+
 			return "<a class='" . ($object->getAccount() ? '' : 'disabled') . " $btnSecondary' target='_blank' href='$link'><i class='fa fa-sign-in-alt'></i></a>";
 		}, '%s', null, ['class' => 'minimal']);
-		
+
 		$grid->addColumn('', function (Customer $object, Datagrid $datagrid) use ($btnSecondary) {
 			return $object->getAccount() != null ?
 				"<a class='$btnSecondary' href='" . $datagrid->getPresenter()->link('editAccount', $object) . "'>Detail&nbsp;účtu</a>" :
 				"<a class='$btnSecondary' href='" . $datagrid->getPresenter()->link('newAccount', $object) . "'>Vytvořit&nbsp;účet</a>";
 		}, '%s', null, ['class' => 'minimal']);
-		
+
 		$grid->addColumnLink('editAddress', 'Adresy');
 		$grid->addColumnLinkDetail('edit');
-		
+
 		$grid->addColumnActionDelete([$this->accountFormFactory, 'deleteAccountHolder']);
-		
+
 		$grid->addButtonSaveAll();
 		$grid->addButtonDeleteSelected([$this->accountFormFactory, 'deleteAccountHolder']);
-		
+
 		$grid->addButtonBulkEdit('form', ['pricelists', 'merchant', 'group'], 'customers');
-		
+
 		$submit = $grid->getForm()->addSubmit('downloadEmails', 'Export e-mailů');
 		$submit->setHtmlAttribute('class', 'btn btn-sm btn-outline-primary');
 		$submit->onClick[] = [$this, 'exportCustomers'];
-		
+
 		$grid->addFilterTextInput('search', ['this.fullname', 'this.email', 'this.phone'], null, 'Jméno a příjmení, email, telefon');
-		
+
 		if (\count($this->merchantRepository->getListForSelect()) > 0) {
 			$grid->addFilterDataMultiSelect(function (ICollection $source, $value) {
 				$source->where('fk_merchant', $value);
 			}, '', 'merchant', 'Obchodník', $this->merchantRepository->getListForSelect(), ['placeholder' => '- Obchodník -']);
 		}
-		
+
 		if (\count($this->groupsRepo->getListForSelect()) > 0) {
 			$grid->addFilterDataMultiSelect(function (ICollection $source, $value) {
 				$source->where('fk_group', $value);
 			}, '', 'group', 'Skupina', $this->groupsRepo->getListForSelect(), ['placeholder' => '- Skupina -']);
 		}
-		
+
 		$grid->addFilterCheckboxInput('newsletter', "newsletter = 1", 'Newsletter');
-		
+
 		$grid->addFilterButtons();
-		
+
 		return $grid;
 	}
-	
+
 	public function exportCustomers(Button $button)
 	{
 		/** @var \Grid\Datagrid $grid */
 		$grid = $button->lookup(Datagrid::class);
-		
+
 		$tempFilename = \tempnam($this->tempDir, "csv");
 		$collection = $grid->getSource()->where($grid->getSourceIdName(), $grid->getSelectedIds());
 		$this->customerRepository->csvExport($collection, Writer::createFromPath($tempFilename, 'w+'));
-		
+
 		$response = new FileResponse($tempFilename, "zakaznici.csv", 'text/csv');
 		$this->sendResponse($response);
 	}
-	
+
 	public function handleLoginCustomer($login)
 	{
 		$this->user->login($login, '', [Customer::class], true);
-		
+
 		$this->presenter->redirect(':Web:Index:default');
 	}
-	
+
 	public function actionEditAccount(Customer $customer): void
 	{
 		/** @var Form $form */
 		$form = $this->getComponent('accountForm');
 		$form['account']['email']->setDefaultValue($customer->email);
 		$form['account']->setDefaults($customer->account->toArray());
-		
+
 		$this->accountFormFactory->onDeleteAccount[] = function () {
 			$this->flashMessage('Účet byl smazán', 'success');
 			$this->redirect('default');
 		};
 	}
-	
+
 	public function actionNewAccount(Customer $customer)
 	{
 		$form = $this->getComponent('accountForm');
 		$form['account']['password']->setRequired();
 		unset($form['delete']);
-		
+
 		$this->accountFormFactory->onCreateAccount[] = function (Account $account) use ($customer) {
 			$customer->update(['account' => $account]);
-			
+
 			$this->flashMessage('Účet byl vytvořen', 'success');
 			$this->redirect('default');
 		};
 	}
-	
+
 	public function renderNewAccount(Customer $customer): void
 	{
 		$this->template->headerLabel = 'Nový účet zákazníka';
@@ -189,11 +193,11 @@ class CustomerPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('accountForm')];
 	}
-	
+
 	public function createComponentForm()
 	{
 		$form = $this->formFactory->create();
-		
+
 		$form->addText('fullname', 'Jméno a příjmení');
 		$form->addText('phone', 'Telefon');
 		$form->addText('company', 'Firma');
@@ -202,7 +206,7 @@ class CustomerPresenter extends BackendPresenter
 		$form->addText('email', 'E-mail')->addRule($form::EMAIL)->setRequired();
 		$form->addText('ccEmails', 'Kopie emailů')->setHtmlAttribute('data-info', 'Zadejte emailové adresy oddělené středníkem (;).');
 		$form->addCheckbox('newsletter', 'Přihlášen k newsletteru');
-		
+
 		$form->addDataMultiSelect('pricelists', 'Ceníky', $this->pricelistRepo->many()->toArrayOf('name'))
 			->setHtmlAttribute('placeholder', 'Vyberte položky...');
 		$form->addDataSelect('merchant', 'Obchodník', $this->merchantRepository->getListForSelect())->setPrompt('Žádná');
@@ -229,30 +233,30 @@ class CustomerPresenter extends BackendPresenter
 			->setHtmlAttribute('Bude použito při exportu objednávky do formátu EDI.');
 		$form->addText('ediBranch', 'EDI: Identifikátor pobočky')
 			->setHtmlAttribute('Bude použito při exportu objednávky do formátu EDI.');
-		
+
 		//$form->bind($this->customerRepository->getStructure(), []);
-		
+
 		if (!$this->getParameter('customer')) {
 			$form->addGroup('Účet');
 			$this->accountFormFactory->addContainer($form);
 		}
-		
+
 		$form->addSubmits(!$this->getParameter('customer'));
-		
+
 		return $form;
 	}
-	
+
 	public function createComponentEditAddress()
 	{
 		$form = $this->formFactory->create();
-		
+
 		$form->addGroup('Fakturační adresa');
 		$billAddress = $form->addContainer('billAddress');
 		$billAddress->addText('street', 'Ulice');
 		$billAddress->addText('city', 'Město');
 		$billAddress->addText('zipcode', 'PSČ');
 		$billAddress->addText('state', 'Stát');
-		
+
 		$form->addGroup('Doručovací adresa');
 		$deliveryAddress = $form->addContainer('deliveryAddress');
 		$deliveryAddress->addText('name', ' Jméno a příjmení / název firmy');
@@ -260,14 +264,14 @@ class CustomerPresenter extends BackendPresenter
 		$deliveryAddress->addText('city', 'Město');
 		$deliveryAddress->addText('zipcode', 'PSČ');
 		$deliveryAddress->addText('state', 'Stát');
-		
+
 		$form->bind(null, ['deliveryAddress' => $this->addressRepo->getStructure(), 'billAddress' => $this->addressRepo->getStructure()]);
-		
+
 		$form->addSubmits();
-		
+
 		return $form;
 	}
-	
+
 	public function renderDefault(): void
 	{
 		$this->template->headerLabel = 'Zákazníci';
@@ -277,7 +281,7 @@ class CustomerPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createNewItemButton('new')];
 		$this->template->displayControls = [$this->getComponent('customers')];
 	}
-	
+
 	public function renderNew(): void
 	{
 		$this->template->headerLabel = 'Nový zákazník';
@@ -288,7 +292,7 @@ class CustomerPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('form')];
 	}
-	
+
 	public function renderEdit(): void
 	{
 		$this->template->headerLabel = 'Detail zákazníka - ' . $this->getParameter('customer')->fullname;
@@ -299,7 +303,7 @@ class CustomerPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('form')];
 	}
-	
+
 	public function renderEditAddress(): void
 	{
 		$this->template->headerLabel = 'Detail adresy zákazníka - ' . $this->getParameter('customer')->fullname;
@@ -310,7 +314,7 @@ class CustomerPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('editAddress')];
 	}
-	
+
 	public function renderEditAccount(Customer $customer): void
 	{
 		$this->template->headerLabel = 'Detail účtu zákazníka - ' . $customer->fullname;
@@ -321,75 +325,81 @@ class CustomerPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('accountForm')];
 	}
-	
+
 	public function actionNew()
 	{
 		/** @var Form $form */
 		$form = $this->getComponent('form');
 		$form['account']['password']->setRequired();
 		$form['account']['passwordCheck']->setRequired();
-		
+
 		$form->onSuccess[] = function (AdminForm $form) {
 			$values = $form->getValues('array');
 			$form['account']['email']->setValue($values['email']);
-			
+
 			unset($values['account']);
-			
+
 			$customer = $this->customerRepository->syncOne($values, null, true);
 			$this->accountFormFactory->onCreateAccount[] = function ($account) use ($customer) {
 				$customer->update(['account' => $account]);
 			};
 			$this->accountFormFactory->success($form, 'register.successAdmin');
-			
-			
+
+
 			$this->flashMessage('Vytvořeno', 'success');
 			$form->processRedirect('edit', 'default', [$customer]);
 		};
 	}
-	
+
 	public function actionEdit(Customer $customer)
 	{
 		/** @var \Forms\Form $form */
 		$form = $this->getComponent('form');
-		
+
 		$form->setDefaults($customer->toArray(['pricelists', 'exclusivePaymentTypes', 'exclusiveDeliveryTypes']));
-		
+
 		$form->onSuccess[] = function (AdminForm $form) use ($customer) {
 			$values = $form->getValues('array');
-			$this->customerRepository->syncOne($values, null ,true);
+
+			$customer = $this->customerRepository->syncOne($values, null, true);
+
+			if ($values['newsletter']) {
+				$this->mailerLite->subscribe($customer);
+			}
+
 			$form->getPresenter()->flashMessage('Uloženo', 'success');
 			$form->processRedirect('edit', 'default', [$customer]);
 		};
-		
+
 		$this->renderEdit();
 	}
-	
+
 	public function actionEditAddress(Customer $customer): void
 	{
 		/** @var \Forms\Form $form */
 		$form = $this->getComponent('editAddress');
-		
+
 		$form->setDefaults($customer->jsonSerialize());
-		
+
 		$form->onSuccess[] = function (AdminForm $form) use ($customer) {
 			$values = $form->getValues('array');
-			
+
 			$bill = $this->addressRepo->syncOne($values['billAddress']);
 			$delivery = $this->addressRepo->syncOne($values['deliveryAddress']);
-			
+
 			$customer->update([
 				'billAddress' => $bill,
 				'deliveryAddress' => $delivery,
 			]);
-			
+
 			$form->getPresenter()->flashMessage('Uloženo', 'success');
-			
+
 			$form->processRedirect('this', 'default');
 		};
-		
+
 		$this->renderEditAddress();
 	}
-	
+
 	public function createComponentAccountForm(): AdminForm
 	{
 		return $this->accountFormFactory->create();
--- src/Integration/MailerLite.php
+++ src/Integration/MailerLite.php
@@ -1,16 +1,45 @@
 <?php
 
-
 namespace Eshop\Integration;
 
-
+use Eshop\Shopper;
+use MailerLiteApi\MailerLite as MailerLiteApi;
+use Eshop\DB\Customer;
 use Web\DB\SettingRepository;
 
 class MailerLite
 {
-	private string $apiKey;
+	private ?string $apiKey;
 
-	public function __construct(SettingRepository $settingRepository)
+	private Shopper $shopper;
+
+	public function __construct(SettingRepository $settingRepository, Shopper $shopper)
 	{
+		if ($apiKey = $settingRepository->many()->where('name = "mailerLiteApiKey"')->first()) {
+			$this->apiKey = $apiKey->value;
+		}
+
+		$this->shopper = $shopper;
+	}
+
+	public function subscribe(Customer $user)
+	{
+		if(!$this->apiKey){
+			throw new \Exception('Missing API key! Set it in Admin.');
+		}
+
+		$groupsApi = (new MailerLiteApi($this->apiKey))->groups();
+
+		$subscriber = [
+			'email' => 'john@example.com',
+			'fields' => [
+				'name' => 'John',
+				'last_name' => 'Doe',
+				'company' => 'John Doe Co.'
+			]
+		];
+
+		$response = $groupsApi->addSubscriber($this->shopper->getProjectUrl(), $subscriber); // Change GROUP_ID with ID of group you want to add subscriber to
+
 	}
 }
