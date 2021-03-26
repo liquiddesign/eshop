cfe6d31d38a67910573e943e04c5d13dff77fd17
Oldrich <oldrich@lqd.cz>
2021-03-26 09:52:14 +0100

    onDeletePage

--- src/Admin/ProductPresenter.php
+++ src/Admin/ProductPresenter.php
@@ -25,7 +25,6 @@ use Nette\Forms\Controls\TextInput;
 use Nette\InvalidArgumentException;
 use Nette\Utils\FileSystem;
 use Nette\Utils\Image;
-use Nette\Utils\Strings;
 use Pages\DB\PageRepository;
 use StORM\DIConnection;
 
@@ -33,55 +32,55 @@ class ProductPresenter extends BackendPresenter
 {
 	/** @inject */
 	public ProductGridFactory $productGridFactory;
-
+	
 	/** @inject */
 	public IProductFormFactory $productFormFatory;
-
+	
 	/** @inject */
 	public IProductParametersFormFactory $productParametersFormFatory;
-
+	
 	/** @inject */
 	public PhotoRepository $photoRepository;
-
+	
 	/** @inject */
 	public FileRepository $fileRepository;
-
+	
 	/** @inject */
 	public PricelistRepository $pricelistRepository;
-
+	
 	/** @inject */
 	public PriceRepository $priceRepository;
-
+	
 	/** @inject */
 	public ParameterRepository $parameterRepository;
-
+	
 	/** @inject */
 	public ParameterValueRepository $parameterValueRepository;
-
+	
 	/** @inject */
 	public VatRateRepository $vatRateRepository;
-
+	
 	/** @inject */
 	public PageRepository $pageRepository;
-
+	
 	/** @inject */
 	public ProductRepository $productRepository;
-
+	
 	public function createComponentProductGrid()
 	{
 		return $this->productGridFactory->create();
 	}
-
+	
 	public function createComponentProductForm()
 	{
 		return $this->productFormFatory->create($this->getParameter('product'));
 	}
-
+	
 	public function createComponentPhotoGrid()
 	{
 		$grid = $this->gridFactory->create($this->photoRepository->many()->where('fk_product', $this->getParameter('product')->getPK()), 20, 'priority', 'ASC', true);
 		$grid->addColumnImage('fileName', Photo::IMAGE_DIR);
-
+		
 		$grid->addColumnText('Popisek', 'label_cs', '%s', 'label_cs');
 		$grid->addColumnInputInteger('Priorita', 'priority', '', '', 'priority', [], true);
 		$grid->addColumnInputCheckbox('<i title="Skryto" class="far fa-eye-slash"></i>', 'hidden', '', '', 'hidden');
@@ -89,38 +88,38 @@ class ProductPresenter extends BackendPresenter
 		$grid->addColumnActionDelete([$this, 'deleteGalleryPhoto']);
 		$grid->addFilterTextInput('search', ['fileName'], null, 'Název');
 		$grid->addFilterButtons(['productPhotos', $this->getParameter('product')]);
-
+		
 		$grid->addButtonSaveAll();
-
+		
 		return $grid;
 	}
-
+	
 	public function createComponentFileGrid()
 	{
 		$grid = $this->gridFactory->create($this->fileRepository->many()->where('fk_product', $this->getParameter('product')->getPK()), 20, 'priority', 'ASC', true);
 		$grid->addColumnSelector();
 		$grid->addColumnText('Popisek', 'label_cs', '%s', 'label_cs');
-
+		
 		$grid->addColumnInputInteger('Priorita', 'priority', '', '', 'priority', [], true);
 		$grid->addColumnInputCheckbox('<i title="Skryto" class="far fa-eye-slash"></i>', 'hidden', '', '', 'hidden');
-
+		
 		$grid->addColumnLinkDetail('detailFile');
-
+		
 		$grid->addColumnActionDelete([$this, 'deleteFile']);
-
+		
 		$grid->addButtonSaveAll();
-
+		
 		$grid->addFilterTextInput('search', ['fileName'], null, 'Jmébo souboru');
 		$grid->addFilterButtons(['productFiles', $this->getParameter('product')]);
-
+		
 		return $grid;
 	}
-
+	
 	public function createComponentPriceGrid()
 	{
 		$product = $this->getParameter('product');
 		$countryCode = 'CZ';
-
+		
 		$collection = $this->pricelistRepository->many()
 			->select([
 				'price' => 'prices.price',
@@ -131,10 +130,10 @@ class ProductPresenter extends BackendPresenter
 			])
 			->join(['prices' => 'eshop_price'], 'prices.fk_pricelist=this.uuid AND prices.fk_product=:product', ['product' => $product])
 			->join(['rates' => 'eshop_vatRate'], 'rates.uuid = :rate AND rates.fk_country=this.fk_country', ['rate' => $product->vatRate]);
-
-
+		
+		
 		$grid = $this->gridFactory->create($collection, 20, 'code', 'ASC');
-
+		
 		$grid->addColumnText('Kód', 'code', '%s', 'code');
 		$grid->addColumnText('Ceník', 'name', '%s', 'name');
 		$grid->addColumnText('Měna', 'currency.code', '%s', 'currency.code');
@@ -142,7 +141,7 @@ class ProductPresenter extends BackendPresenter
 		$grid->addColumnInputPrice('Cena s DPH', 'priceVat');
 		$grid->addColumnInputPrice('Původní', 'priceBefore');
 		$grid->addColumnInputPrice('Původní s DPH', 'priceVatBefore');
-
+		
 		$submit = $grid->getForm()->addSubmit('submit', 'Uložit');
 		$submit->setHtmlAttribute('class', 'btn btn-sm btn-primary');
 		$submit->onClick[] = function ($button) use ($grid, $product) {
@@ -150,7 +149,7 @@ class ProductPresenter extends BackendPresenter
 				if (!isset($data['price'])) {
 					continue;
 				}
-
+				
 				$this->priceRepository->syncOne([
 					'price' => (float)$data['price'],
 					'priceVat' => isset($data['priceVat']) ? (float)$data['priceVat'] : null,
@@ -160,54 +159,59 @@ class ProductPresenter extends BackendPresenter
 					'pricelist' => $id,
 				]);
 			}
-
+			
 			$grid->getPresenter()->flashMessage('Uloženo', 'success');
 			$grid->getPresenter()->redirect('this');
 		};
-
+		
 		$grid->addFilterTextInput('search', ['code'], null, 'Kód ceníku');
 		$grid->addFilterButtons(['productFiles', $this->getParameter('product')]);
-
+		
 		return $grid;
 	}
-
+	
 	public function createComponentFileForm(): Form
 	{
 		$form = $this->formFactory->create();
-		$form->addFilePicker('fileName', 'Vybrat soubor', \DIRECTORY_SEPARATOR . Product::FILE_DIR)->setRequired();
-
+		
+		if (!$this->getParameter('file')) {
+			$form->addFilePicker('fileName', 'Vybrat soubor', \DIRECTORY_SEPARATOR . Product::FILE_DIR)->setRequired();
+		}
+		
 		$form->addLocaleText('label', 'Popisek');
 		$form->addInteger('priority', 'Priorita')->setDefaultValue(10);
 		$form->addCheckbox('hidden', 'Skryto');
-
+		
 		$form->addHidden('product', (string)$this->getParameter('product'));
-
+		
 		$form->addSubmits(false, false);
-
+		
 		$form->onSuccess[] = function (Form $form) {
 			$values = $form->getValues('array');
-
+			
 			$this->createDirectories();
-
+			
 			if (!$values['uuid']) {
 				$values['uuid'] = DIConnection::generateUuid();
 			}
-
-			$values['fileName'] = $form['fileName']->upload($values['uuid'] . '.%2$s');
-
+			
+			if (isset($values['fileName'])) {
+				$values['fileName'] = $form['fileName']->upload($values['uuid'] . '.%2$s');
+			}
+			
 			$file = $this->fileRepository->syncOne($values);
-
+			
 			$this->flashMessage('Uloženo', 'success');
-			$form->processRedirect('fileDetail', 'files', [$file, $this->getParameter('product')], [$this->getParameter('product')]);
+			$this->redirect('files', [new Product(['uuid' => $values['product']])]);
 		};
-
+		
 		return $form;
 	}
-
+	
 	public function createComponentPhotoForm(): Form
 	{
 		$form = $this->formFactory->create();
-
+		
 		$imagePicker = $form->addImagePicker('fileName', 'Obrázek', [
 			Product::GALLERY_DIR . \DIRECTORY_SEPARATOR . 'origin' => null,
 			Product::GALLERY_DIR . \DIRECTORY_SEPARATOR . 'detail' => static function (Image $image): void {
@@ -217,9 +221,9 @@ class ProductPresenter extends BackendPresenter
 				$image->resize(300, null);
 			},
 		]);
-
+		
 		$form->addLocaleText('label', 'Popisek');
-
+		
 		/*$imagePicker->onDelete[] = function (array $directories, $filename) use ($product) {
 			foreach ($directories as $key => $directory) {
 				FileSystem::delete($this->wwwDir . \DIRECTORY_SEPARATOR . 'userfiles' . \DIRECTORY_SEPARATOR . $key . \DIRECTORY_SEPARATOR . $photo->fileName);
@@ -228,43 +232,46 @@ class ProductPresenter extends BackendPresenter
 			$photo->delete();
 			$this->redirect('productPhotos', $product);
 		};*/
-
+		
 		$form->addInteger('priority', 'Priorita')->setDefaultValue(10);
 		$form->addCheckbox('hidden', 'Skryto');
-		$form->addHidden('product', (string)$this->getParameter('product'));
-
+		$form->addHidden('product', (string) ($this->getParameter('photo') ? $this->getParameter('photo')->product : $this->getParameter('product')));
+		
 		$form->addSubmits(false, false);
-
+		
 		$form->onSuccess[] = function (AdminForm $form) {
 			$values = $form->getValues('array');
-
+			
+			$this->createDirectories();
+			
 			if (!$values['uuid']) {
 				$values['uuid'] = DIConnection::generateUuid();
 			}
-
+			
 			$values['fileName'] = $form['fileName']->upload($values['uuid'] . '.%2$s');
-
+			
 			$photo = $this->photoRepository->syncOne($values);
-
+			
 			$this->flashMessage('Uloženo', 'success');
-			$form->processRedirect('photoDetail', 'photos', [$photo], [$this->getParameter('product')]);
+			$this->redirect('photos', [new Product(['uuid' => $values['product']])]);
+			
 		};
-
+		
 		return $form;
 	}
-
+	
 	public function createComponentParameterForm()
 	{
 		return $this->productParametersFormFatory->create($this->getParameter('product'));
 	}
-
+	
 	public function actionDetailFile(File $file)
 	{
 		/** @var \Forms\Form $form */
 		$form = $this->getComponent('fileForm');
 		$form->setDefaults($file->toArray());
 	}
-
+	
 	public function renderDetailFile(File $file)
 	{
 		$this->template->headerLabel = 'Nová položka';
@@ -276,7 +283,7 @@ class ProductPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('files', $file->product)];
 		$this->template->displayControls = [$this->getComponent('fileForm')];
 	}
-
+	
 	public function renderNewFile(Product $product)
 	{
 		$this->template->headerLabel = 'Nová položka';
@@ -288,7 +295,7 @@ class ProductPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('files', $product)];
 		$this->template->displayControls = [$this->getComponent('fileForm')];
 	}
-
+	
 	public function renderDetailPhoto(Photo $photo)
 	{
 		$this->template->headerLabel = 'Nová položka';
@@ -300,7 +307,7 @@ class ProductPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('photos', $photo->product)];
 		$this->template->displayControls = [$this->getComponent('photoForm')];
 	}
-
+	
 	public function renderNewPhoto(Product $product)
 	{
 		$this->template->headerLabel = 'Nová položka';
@@ -312,7 +319,7 @@ class ProductPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('photos', $product)];
 		$this->template->displayControls = [$this->getComponent('photoForm')];
 	}
-
+	
 	public function renderNew()
 	{
 		$this->template->headerLabel = 'Detail';
@@ -325,12 +332,20 @@ class ProductPresenter extends BackendPresenter
 			$this->getComponent('productForm'),
 		];
 	}
-
+	
+	public function actionDetailPhoto(Photo $photo)
+	{
+		/** @var \Forms\Form $form */
+		$form = $this->getComponent('photoForm');
+		$form->setDefaults($photo->toArray());
+	}
+	
+	
 	public function actionEdit(Product $product)
 	{
 		/** @var \Forms\Form $form */
 		$form = $this->getComponent('productForm')['form'];
-
+		
 		$prices = $this->pricelistRepository->getDefaultPricelists()
 			->join(['prices' => 'eshop_price'], 'prices.fk_product=:product AND prices.fk_pricelist=this.uuid', ['product' => $product])
 			->select([
@@ -339,13 +354,13 @@ class ProductPresenter extends BackendPresenter
 				'priceBefore' => 'prices.priceBefore',
 				'priceVatBefore' => 'prices.priceVatBefore',
 			])->toArray();
-
+		
 		foreach ($form['prices']->getComponents() as $pricelistId => $container) {
 			$container->setDefaults($prices[$pricelistId]->toArray());
 		}
-
+		
 		$form->setDefaults($product->toArray(['categories', 'tags', 'ribbons', 'parameterGroups', 'taxes']));
-
+		
 		if (isset($form['tonerForPrinters'])) {
 			try {
 				$form['tonerForPrinters']->setDefaultValue($this->productRepository->getSlaveProductsByRelationAndMaster('tonerForPrinter', $product)->setSelect(['this.uuid'])->toArray());
@@ -353,17 +368,17 @@ class ProductPresenter extends BackendPresenter
 				$form['tonerForPrinters']->setHtmlAttribute('data-error', 'Byla detekována chybná vazba! Vyberte, prosím, tiskárny znovu.');
 			}
 		}
-
+		
 		if ($page = $this->pageRepository->getPageByTypeAndParams('product_detail', null, ['product' => $product])) {
 			$form['page']->setDefaults($page->toArray());
-
+			
 			$form['page']['url']->forAll(function (TextInput $text, $mutation) use ($page, $form) {
 				$text->getRules()->reset();
 				$text->addRule([$form, 'validateUrl'], 'URL již existuje', [$this->pageRepository, $mutation, $page->getPK()]);
 			});
 		}
 	}
-
+	
 	public function renderEdit(Product $product)
 	{
 		$this->template->headerLabel = 'Detail';
@@ -376,12 +391,12 @@ class ProductPresenter extends BackendPresenter
 			$this->getComponent('productForm'),
 		];
 	}
-
+	
 	public function actionParameters(Product $product)
 	{
-
+	
 	}
-
+	
 	public function renderParameters(Product $product)
 	{
 		$this->template->headerLabel = 'Parametery - ' . $product->name;
@@ -392,7 +407,7 @@ class ProductPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('parameterForm')];
 	}
-
+	
 	public function renderPrices(Product $product)
 	{
 		$this->template->headerLabel = 'Ceny - ' . $product->name;
@@ -403,7 +418,7 @@ class ProductPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('default')];
 		$this->template->displayControls = [$this->getComponent('priceGrid')];
 	}
-
+	
 	public function renderPhotos(Product $product)
 	{
 		$this->template->headerLabel = 'Fotografie - ' . $product->name;
@@ -414,7 +429,7 @@ class ProductPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('default'), $this->createNewItemButton('newPhoto', [$product])];
 		$this->template->displayControls = [$this->getComponent('photoGrid')];
 	}
-
+	
 	public function renderFiles(Product $product)
 	{
 		$this->template->headerLabel = 'Soubory - ' . $product->name;
@@ -425,7 +440,7 @@ class ProductPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createBackButton('default'), $this->createNewItemButton('newFile', [$product])];
 		$this->template->displayControls = [$this->getComponent('fileGrid')];
 	}
-
+	
 	public function renderDefault(): void
 	{
 		$this->template->headerLabel = 'Produkty';
@@ -435,54 +450,41 @@ class ProductPresenter extends BackendPresenter
 		$this->template->displayButtons = [$this->createNewItemButton('new')];
 		$this->template->displayControls = [$this->getComponent('productGrid')];
 	}
-
+	
 	public function deleteFile(File $file)
 	{
 		$dir = File::FILE_DIR;
 		$rootDir = $this->wwwDir . \DIRECTORY_SEPARATOR . 'userfiles' . \DIRECTORY_SEPARATOR . $dir;
 		FileSystem::delete($rootDir . \DIRECTORY_SEPARATOR . $file->fileName);
 	}
-
+	
 	public function deleteGalleryPhoto(Photo $photo)
 	{
 		$subDirs = ['origin', 'detail', 'thumb'];
 		$dir = Photo::IMAGE_DIR;
-
+		
 		foreach ($subDirs as $subDir) {
 			$rootDir = $this->wwwDir . \DIRECTORY_SEPARATOR . 'userfiles' . \DIRECTORY_SEPARATOR . $dir;
 			FileSystem::delete($rootDir . \DIRECTORY_SEPARATOR . $subDir . \DIRECTORY_SEPARATOR . $photo->fileName);
 		}
-
+		
 		$photo->update(['fileName' => null]);
 	}
-
+	
 	protected function createDirectories()
 	{
 		$subDirs = ['origin', 'detail', 'thumb'];
-		$dirs = [Product::IMAGE_DIR, Photo::IMAGE_DIR];
-
+		$dirs = [Product::IMAGE_DIR, Photo::IMAGE_DIR, Product::FILE_DIR];
+		
 		foreach ($dirs as $dir) {
 			$rootDir = $this->wwwDir . \DIRECTORY_SEPARATOR . 'userfiles' . \DIRECTORY_SEPARATOR . $dir;
 			FileSystem::createDir($rootDir);
-
+			
 			foreach ($subDirs as $subDir) {
 				FileSystem::createDir($rootDir . \DIRECTORY_SEPARATOR . $subDir);
 			}
 		}
-
-		FileSystem::createDir($this->wwwDir . \DIRECTORY_SEPARATOR . 'userfiles' . \DIRECTORY_SEPARATOR . File::FILE_DIR);
-	}
-	
-	public function getDataJson(): \stdClass
-	{
-		$object = new \stdClass();
-		$object->name = Strings::fixEncoding($this->name);
-		$object->id = $this->getPK();
-		$object->price = $this->getPrice();
-		$object->producer = $this->producer ? Strings::toAscii($this->producer->name) : null;
-		$object->category = $this->primaryCategory ? Strings::fixEncoding($this->primaryCategory->name) : null;
-		$object->variant = null;
 		
-		return $object;
+		FileSystem::createDir($this->wwwDir . \DIRECTORY_SEPARATOR . 'userfiles' . \DIRECTORY_SEPARATOR . File::FILE_DIR);
 	}
 }
