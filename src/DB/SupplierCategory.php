4ad88f3205579a4495be6d7b95b8d158b7039fe0
Oldrich <oldrich@lqd.cz>
2021-04-10 14:24:26 +0200

    v2

--- src/DB/SupplierCategory.php
+++ src/DB/SupplierCategory.php
@@ -7,15 +7,27 @@ namespace Eshop\DB;
 /**
  * Mapování kategorií
  * @table
- * @index{"name":"supplier_category_name","unique":true,"columns":["name","fk_supplier"]}
+ * @index{"name":"supplier_category_name","unique":true,"columns":["categoryNameL1","categoryNameL2","categoryNameL3","fk_supplier"]}
  */
 class SupplierCategory extends \StORM\Entity
 {
 	/**
-	 * Vzor na mapování
+	 * Vzor na mapování 1
 	 * @column
 	 */
-	public string $name;
+	public string $categoryNameL1;
+	
+	/**
+	 * Vzor na mapování 2
+	 * @column
+	 */
+	public ?string $categoryNameL2;
+	
+	/**
+	 * Vzor na mapování 3
+	 * @column
+	 */
+	public ?string $categoryNameL3;
 	
 	/**
 	 * Mapování kategorií, jestli je zadáno
