4513803d96d82259adba4d63886474b6f19c38c5
Oldrich <oldrich@lqd.cz>
2021-04-10 14:26:05 +0200

    v2

--- src/DB/Supplier.php
+++ src/DB/Supplier.php
@@ -35,39 +35,20 @@ class Supplier extends \StORM\Entity
 	public ?string $phone;
 	
 	/**
-	 * Dodavatel na kterém je web závislý
-	 * @column
-	 */
-	public bool $systemic = false;
-	
-	/**
 	 * Priorita importu
 	 * @column
 	 */
 	public int $importPriority = 0;
 	
 	/**
+	 * Procentuální změna cen
+	 * @column
+	 */
+	public int $importPriceRatio = 100;
+	
+	/**
 	 * Je importu aktivní
 	 * @column
 	 */
 	public ?bool $isImportActive;
-	
-	/**
-	 * Dodací adresa
-	 * @relation
-	 * @constraint
-	 */
-	public ?Address $deliveryAddress;
-	
-	/**
-	 * Fakturační adresa
-	 * @relation
-	 * @constraint
-	 */
-	public ?Address $billAddress;
-	
-	public function isSystemic(): bool
-	{
-		return $this->systemic;
-	}
 }
