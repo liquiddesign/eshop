01dbf1547c2f952f59300654ed64511a056f5c2b
Petr Křehlík <petr@lqd.cz>
2021-05-28 12:21:11 +0200

    attribute

--- src/DB/AttributeRepository.php
+++ src/DB/AttributeRepository.php
@@ -80,7 +80,7 @@ class AttributeRepository extends \StORM\Repository implements IGeneralRepositor
 			->join(['attributeAssign' => 'eshop_attributeassign'], 'attributeAssign.fk_value = attributeValue.uuid')
 			->join(['product' => $collection], 'product.uuid=attributeAssign.fk_product', $collection->getVars())
 			->join(['attributeXcategory' => 'eshop_attribute_nxn_eshop_category'], 'this.uuid = attributeXcategory.fk_attribute')
-			->where('attributeXcategory.fk_category', Arrays::last(\array_keys($categories)))
+			->where('attributeXcategory.fk_category', \array_keys($categories))
 			->setSelect(['count' => 'COUNT(product.uuid)'])
 			->setIndex('attributeValue.uuid')
 			->setGroupBy(['attributeValue.uuid']);
