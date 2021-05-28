65b757051026b2f39bf5a2f1f181ed13d3f3ef5e
Petr Křehlík <petr@lqd.cz>
2021-05-28 13:58:24 +0200

    attribute

--- src/CompareManager.php
+++ src/CompareManager.php
@@ -6,7 +6,6 @@ namespace Eshop;
 use Eshop\DB\AttributeRepository;
 use Eshop\DB\AttributeValueRepository;
 use Eshop\DB\CategoryRepository;
-use Eshop\DB\ParameterCategory;
 use Eshop\DB\ProductRepository;
 use Nette\Http\Session;
 use Nette\Http\SessionSection;
@@ -56,102 +55,43 @@ class CompareManager
 		return $section;
 	}
 
-	public function getProductsInCategory(ParameterCategory $parameterCategory): array
+	public function getAttributes(?string $category = null): array
 	{
-		return $this->getCompareList()[$parameterCategory->getPK()] ?? [];
+		$parsedProducts = $this->getParsedProductsWithPrimaryCategories($category);
+
+		return $parsedProducts;
 	}
 
-	/**
-	 * @param string|null $parameterCategory
-	 * @return array contains asociative arrays with parameter and parameter values for products
-	 * @throws \StORM\Exception\NotFoundException
-	 */
-	public function getParameters(?string $parameterCategory): array
+	public function getParsedProductsWithPrimaryCategories(?string $categoryPK = null): array
 	{
-		$categories = $this->getCategories();
+		$resultCategories = [];
 
-		/** @var \Eshop\DB\ParameterCategory $parameterCategory */
-		if (!$parameterCategory && !\Nette\Utils\Arrays::contains($categories, $parameterCategory)) {
-			$parameterCategory = \Nette\Utils\Arrays::first($categories);
-		} else {
-			$parameterCategory = $this->parameterCategoryRepository->one($parameterCategory);
-		}
+		foreach ($this->getCompareList() as $productKey => $product) {
+			$product = $this->productRepository->one($productKey);
 
-		if (!$parameterCategory) {
-			return [];
-		}
-
-		/** @var \Eshop\DB\Product[] $products */
-		$products = $this->getProductsInCategory($parameterCategory);
-
-		/** @var \Eshop\DB\ParameterGroup $groups */
-		$groups = $this->parameterGroupRepository->getCollection()
-			->where('fk_parametercategory', $parameterCategory->getPK())
-			->toArray();
-
-		$data = [
-			'products' => $products,
-			'groups' => $groups,
-			'parameters' => [],
-			'values' => []
-		];
-
-		foreach ($groups as $group) {
-			/** @var \Eshop\DB\Parameter[] $parameters */
-			$parameters = $this->parameterRepository->getCollection()
-				->where('fk_group', $group->getPK())
-				->toArray();
-			$data['parameters'] += $parameters;
-
-			foreach ($parameters as $parameter) {
-				foreach ($products as $product) {
-					/** @var \Eshop\DB\ParameterValue $value */
-					$value = $this->parameterValueRepository->getCollection()
-						->where('fk_product', $product->getPK())
-						->where('fk_parameter', $parameter->getPK())
-						->first();
-
-					if (!($value && ($value->content || $value->metaValue))) {
-						$data['values'][$group->getPK()][$parameter->getPK()][$product->getPK()] = '-';
-						continue;
-					}
-
-					if($value->parameter->type == 'bool'){
-						$data['values'][$group->getPK()][$parameter->getPK()][$product->getPK()] = $value->metaValue ? $this->translator->translate('.yes', 'Ano') : $this->translator->translate('.no', 'Ne');
-					}elseif ($value->parameter->type == 'list'){
-						$allowed = \array_combine(\explode(';', $parameter->allowedKeys ?? ''), \explode(';', $parameter->allowedValues ?? ''));
-						$metaValue = \explode(';', $value->metaValue);
-						$finalMeta = [];
-						foreach ($metaValue as $metaV){
-							$finalMeta[] = $allowed[$metaV];
-						}
-
-						$data['values'][$group->getPK()][$parameter->getPK()][$product->getPK()] = \implode(', ',$finalMeta);
-					}else{
-						$data['values'][$group->getPK()][$parameter->getPK()][$product->getPK()] = $value->content;
-					}
+			if ($category = $product->getPrimaryCategory()) {
+				if (!$categoryPK || $category->getPK() == $categoryPK) {
+					$resultCategories[$category->getPK()]['products'][] = $product;
+					$resultCategories[$category->getPK()]['category'] = $category;
 				}
 			}
 		}
 
-		return $data;
+		return $resultCategories;
 	}
 
-	/**
-	 * @param string|null $parameter
-	 * @return array
-	 * @throws \StORM\Exception\NotFoundException
-	 */
-	public function getCategories(?string $parameter = null): array
+	public function getCategories(?string $parameterName = null): array
 	{
 		$resultCategories = [];
 
-		foreach (\array_keys($this->getCompareList()) as $categoryPK) {
-			if (!$category = $this->parameterCategoryRepository->one($categoryPK)) {
+		foreach ($this->getCompareList() as $productKey => $product) {
+			$product = $this->productRepository->one($productKey);
+
+			if (!$category = $product->getPrimaryCategory()) {
 				continue;
 			}
 
-			$resultCategories[$category->getPK()] = $parameter ? $category->$parameter : $category;
+			$resultCategories[$category->getPK()] = $parameterName ? $category->$parameterName : $category;
 		}
 
 		return $resultCategories;
@@ -171,13 +111,9 @@ class CompareManager
 
 		if (!$productCategory = $product->getPrimaryCategory()) {
 			return;
-		};
-
-		if (!$parameterCategory = $this->categoryRepository->getParameterCategoryOfCategory($productCategory)) {
-			return;
 		}
 
-		$section->list[$parameterCategory->getPK()][$product->getPK()] = $product;
+		$section->list[$product->getPK()] = $product;
 	}
 
 	public function clearCompareList(): void
@@ -189,27 +125,15 @@ class CompareManager
 	{
 		$section = $this->getSessionSection();
 
-		foreach ($section->list as $key => $value) {
-			if (\Nette\Utils\Arrays::contains(\array_keys($value), $searchedProduct)) {
-				unset($section->list[$key][$searchedProduct]);
-
-				if (\count($section->list[$key]) == 0) {
-					unset($section->list[$key]);
-				}
-
-				return;
-			}
-		}
+		unset($section->list[$searchedProduct]);
 	}
 
 	public function isProductInList(string $searchedProduct): bool
 	{
 		$section = $this->getCompareList();
 
-		foreach ($section as $key => $value) {
-			if (\Nette\Utils\Arrays::contains(\array_keys($value), $searchedProduct)) {
-				return true;
-			}
+		if (\Nette\Utils\Arrays::contains(\array_keys($section), $searchedProduct)) {
+			return true;
 		}
 
 		return false;
@@ -219,12 +143,6 @@ class CompareManager
 	{
 		$section = $this->getCompareList();
 
-		$count = 0;
-
-		foreach ($section as $key => $value) {
-			$count += \count($value);
-		}
-
-		return $count;
+		return \count($section);
 	}
 }
