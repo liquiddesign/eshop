9387a012aef8ac1a16bb3d683bff50e73ec29505
Petr Křehlík <petr@lqd.cz>
2021-07-09 13:18:22 +0200

    bought printers fix

--- src/DB/ProductRepository.php
+++ src/DB/ProductRepository.php
@@ -9,6 +9,7 @@ use Eshop\Shopper;
 use League\Csv\Writer;
 use Nette\Utils\Arrays;
 use Pages\Pages;
+use Security\DB\Account;
 use StORM\Collection;
 use StORM\DIConnection;
 use StORM\Entity;
@@ -625,13 +626,16 @@ class ProductRepository extends Repository implements IGeneralRepository
 	}
 
 	/**
-	 * @param \Eshop\DB\Customer|\Eshop\DB\Merchant $user
+	 * @param Account|string $account
 	 * @return \StORM\Collection|\StORM\GenericCollection|array
+	 * @throws \StORM\Exception\NotFoundException
 	 */
-	public function getBoughtPrintersByUser($user)
+	public function getBoughtPrintersByUser($account)
 	{
-		if (!$user instanceof Customer && !$user instanceof Merchant) {
-			return [];
+		if (!$account instanceof Account) {
+			if (!$account = $this->getConnection()->findRepository(Account::class)->one($account)) {
+				return [];
+			}
 		}
 
 		return $this->many()
@@ -640,7 +644,7 @@ class ProductRepository extends Repository implements IGeneralRepository
 			->join(['cart' => 'eshop_cart'], 'cartitem.fk_cart = cart.uuid')
 			->join(['purchase' => 'eshop_purchase'], 'cart.fk_purchase = purchase.uuid')
 			->join(['orderTable' => 'eshop_order'], 'orderTable.fk_purchase = purchase.uuid')
-			->where('orderTable.fk_customer', $user->getPK())
+			->where('purchase.fk_account', $account->getPK())
 			->where('relation.fk_type', 'tonerForPrinter')
 			->where('orderTable.completedTs IS NOT NULL')
 			->orderBy(['orderTable.completedTs' => 'DESC']);
