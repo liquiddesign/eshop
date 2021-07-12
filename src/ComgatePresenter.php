d7984814170b0ed0d91bf3476c38c6442b9a1ee3
Jan Klíma <honza@lqd.cz>
2021-07-12 13:40:33 +0200

    Comgate request

---
+++ src/ComgatePresenter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Eshop;
+
+use Nette\Application\BadRequestException;
+use Nette\Application\UI\Presenter;
+use Tracy\Debugger;
+
+class ComgatePresenter extends Presenter
+{
+	public function actionPaymentResult(): void
+	{
+		if ($this->request->getMethod() !== 'POST') {
+			throw new BadRequestException();
+		}
+		
+		$data = $this->request->getPost();
+		
+		if (!isset($data)) {
+			throw new \Exception('No data from server');
+		}
+		
+		Debugger::log($data);
+		
+		$this->sendJson();
+	}
+}
