1a8ab222355e780815d18ca7c590854533db091c
Petr Křehlík <petr@lqd.cz>
2021-08-09 16:56:48 +0200

    internal comments

--- src/DB/InternalCommentOrderRepository.php
+++ src/DB/InternalCommentOrderRepository.php
@@ -10,15 +10,6 @@ use StORM\Collection;
 /**
  * @extends \StORM\Repository<\Eshop\DB\InternalCommentOrder>
  */
-class InternalCommentOrderRepository extends \StORM\Repository implements IGeneralRepository
+class InternalCommentOrderRepository extends \StORM\Repository
 {
-	public function getArrayForSelect(bool $includeHidden = true): array
-	{
-		return $this->getCollection($includeHidden)->toArrayOf('name');
-	}
-	
-	public function getCollection(bool $includeHidden = false): Collection
-	{
-		return $this->many()->orderBy(['name']);
-	}
 }
--- src/DB/InternalCommentProductRepository.php
+++ src/DB/InternalCommentProductRepository.php
@@ -10,15 +10,6 @@ use StORM\Collection;
 /**
  * @extends \StORM\Repository<\Eshop\DB\InternalCommentProduct>
  */
-class InternalCommentProductRepository extends \StORM\Repository implements IGeneralRepository
+class InternalCommentProductRepository extends \StORM\Repository
 {
-	public function getArrayForSelect(bool $includeHidden = true): array
-	{
-		return $this->getCollection($includeHidden)->toArrayOf('name');
-	}
-	
-	public function getCollection(bool $includeHidden = false): Collection
-	{
-		return $this->many()->orderBy(['name']);
-	}
 }
