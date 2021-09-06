e353cd57b87c0314a68e40d6b50832375eb78af1
Petr Křehlík <petr@lqd.cz>
2021-09-06 09:26:33 +0200

    filter

--- src/DB/ProductRepository.php
+++ src/DB/ProductRepository.php
@@ -422,20 +422,22 @@ class ProductRepository extends Repository implements IGeneralRepository
 			}
 
 			if ($attribute->filterType == 'and') {
-//				if ($attribute->showRange) {
-//					$attributeValues = $this->getConnection()->rows(['eshop_attributevalue'])
-//						->where('eshop_attributevalue.fk_attributevaluerange', $attributeValues)
-//						->where('eshop_attributevalue.fk_attribute', $attribute->getPK())
-//						->toArrayOf('uuid');
-//				}
-
 				foreach ($attributeValues as $attributeValue) {
-					$subSelect = $this->getConnection()->rows(['eshop_attributevalue'])
-						->join(['eshop_attributeassign'], 'eshop_attributeassign.fk_value = eshop_attributevalue.uuid')
-						->join(['eshop_attribute'], 'eshop_attribute.uuid = eshop_attributevalue.fk_attribute')
-						->where('eshop_attributeassign.fk_product=this.uuid')
-						->where("eshop_attributevalue.fk_attribute = '$attributeKey'")
-						->where("eshop_attributevalue.uuid = '$attributeValue'");
+					if ($attribute->showRange) {
+						$subSelect = $this->getConnection()->rows(['eshop_attributevalue'])
+							->join(['eshop_attributeassign'], 'eshop_attributeassign.fk_value = eshop_attributevalue.uuid')
+							->join(['eshop_attribute'], 'eshop_attribute.uuid = eshop_attributevalue.fk_attribute')
+							->where('eshop_attributeassign.fk_product=this.uuid')
+							->where("eshop_attributevalue.fk_attributevaluerange = '$attributeKey'")
+							->where("eshop_attributevalue.uuid = '$attributeValue'");
+					} else {
+						$subSelect = $this->getConnection()->rows(['eshop_attributevalue'])
+							->join(['eshop_attributeassign'], 'eshop_attributeassign.fk_value = eshop_attributevalue.uuid')
+							->join(['eshop_attribute'], 'eshop_attribute.uuid = eshop_attributevalue.fk_attribute')
+							->where('eshop_attributeassign.fk_product=this.uuid')
+							->where("eshop_attributevalue.fk_attribute = '$attributeKey'")
+							->where("eshop_attributevalue.uuid = '$attributeValue'");
+					}
 
 					$collection->where('EXISTS (' . $subSelect->getSql() . ')');
 				}
