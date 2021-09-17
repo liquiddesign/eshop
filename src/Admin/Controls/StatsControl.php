21d14fd1e5d2d25406c7838bbe9e549df52fc63a
Petr Křehlík <petr@lqd.cz>
2021-09-17 16:58:52 +0200

    stats meny

--- src/Admin/Controls/StatsControl.php
+++ src/Admin/Controls/StatsControl.php
@@ -2,6 +2,8 @@
 
 namespace Eshop\Admin\Controls;
 
+use Eshop\DB\Currency;
+use Eshop\DB\CurrencyRepository;
 use Eshop\Shopper;
 use Eshop\DB\MerchantRepository;
 use Eshop\DB\OrderRepository;
@@ -13,13 +15,7 @@ use Nette\Application\UI\Control;
 class StatsControl extends Control
 {
 	/** @persistent */
-	public $statsFrom;
-
-	/** @persistent */
-	public $statsTo;
-
-	/** @persistent */
-	public $merchant;
+	public array $state = [];
 
 	private FormFactory $formFactory;
 
@@ -29,6 +25,8 @@ class StatsControl extends Control
 
 	private MerchantRepository $merchantRepository;
 
+	private CurrencyRepository $currencyRepository;
+
 	private $user;
 
 	/**
@@ -37,13 +35,14 @@ class StatsControl extends Control
 	 * @param \Eshop\Shopper $shopper
 	 * @param \Eshop\DB\Customer|\Eshop\DB\Merchant|null $user
 	 */
-	public function __construct(FormFactory $formFactory, Shopper $shopper, OrderRepository $orderRepository, MerchantRepository $merchantRepository, $user = null)
+	public function __construct(FormFactory $formFactory, Shopper $shopper, OrderRepository $orderRepository, MerchantRepository $merchantRepository, CurrencyRepository $currencyRepository, $user = null)
 	{
 		$this->formFactory = $formFactory;
 		$this->shopper = $shopper;
 		$this->user = $user;
 		$this->orderRepository = $orderRepository;
 		$this->merchantRepository = $merchantRepository;
+		$this->currencyRepository = $currencyRepository;
 	}
 
 	public function createComponentStatsFilterForm(): Form
@@ -53,16 +52,32 @@ class StatsControl extends Control
 		$form->addText('from', 'Od')
 			->setHtmlAttribute('min', (new Nette\Utils\DateTime())->modify('- 1 year')->format('Y-m-d'))
 			->setHtmlAttribute('max', (new Nette\Utils\DateTime())->format('Y-m-d'))
-			->setHtmlType('date');
+			->setHtmlType('date')
+			->setRequired()
+			->setDefaultValue((new Nette\Utils\DateTime())->modify('- 1 year')->format('Y-m-d'));
 		$form->addText('to', 'Do')
 			->setHtmlAttribute('min', (new Nette\Utils\DateTime())->modify('- 1 year')->format('Y-m-d'))
 			->setHtmlAttribute('max', (new Nette\Utils\DateTime())->format('Y-m-d'))
-			->setHtmlType('date');
-		$form->addDataSelect('merchant', 'Obchodník', $this->merchantRepository->getListForSelect())->setPrompt('- Obchodník -');
+			->setHtmlType('date')
+			->setRequired()
+			->setDefaultValue((new Nette\Utils\DateTime())->format('Y-m-d'));
+		$form->addDataSelect('merchant', 'Obchodník', $this->merchantRepository->getArrayForSelect())->setPrompt('- Obchodník -');
+
+		$currencies = $this->currencyRepository->getArrayForSelect();
+
+		$input = $form->addDataSelect('currency', 'Měna', $currencies)->setRequired();
+
+		if (\count($currencies) > 0) {
+			$input->setDefaultValue(Nette\Utils\Arrays::first($currencies));
+		}
 
 		$form->addSubmit('submit', 'Zobrazit');
 
 		$form->onValidate[] = function (Form $form) {
+			if (!$form->isValid()) {
+				return;
+			}
+
 			$values = $form->getValues();
 
 			if ($values->from > $values->to) {
@@ -71,11 +86,7 @@ class StatsControl extends Control
 		};
 
 		$form->onSuccess[] = function (Form $form) {
-			$values = $form->getValues();
-
-			$this->statsFrom = $values->from;
-			$this->statsTo = $values->to;
-			$this->merchant = $values->merchant;
+			$this->state = $form->getValues('array');
 
 			$this->redirect('this');
 		};
@@ -88,26 +99,28 @@ class StatsControl extends Control
 		/** @var Nette\Application\UI\Form $form */
 		$form = $this->getComponent('statsFilterForm');
 
-		$form['from']->setDefaultValue($this->statsFrom ?? (new Nette\Utils\DateTime())->modify('- 1 year')->format('Y-m-d'));
-		$form['to']->setDefaultValue($this->statsTo ?? (new Nette\Utils\DateTime())->format('Y-m-d'));
-		$form['merchant']->setDefaultValue($this->merchant);
+		$statsFrom = $this->state['statsFrom'] ?? null;
+		$statsTo = $this->state['statsTo'] ?? null;
+		$merchant = $this->state['merchant'] ?? null;
+		/** @var Currency $currency */
+		$currency = isset($this->state['currency']) ? $this->currencyRepository->one($this->state['currency'], true) : null;
 
-		$currency = $this->shopper->getCurrency();
+		$form->setDefaults($this->state);
 
-		$user = $this->merchant ? $this->merchantRepository->one($this->merchant) : $this->user;
+		$user = $merchant ? $this->merchantRepository->one($merchant) : $this->user;
 
-		$from = isset($this->statsFrom) ? (new Nette\Utils\DateTime($this->statsFrom)) : ((new Nette\Utils\DateTime())->modify('- 1 year'));
-		$to = isset($this->statsTo) ? (new Nette\Utils\DateTime($this->statsTo)) : (new Nette\Utils\DateTime());
+		$from = $statsFrom ? (new Nette\Utils\DateTime($statsFrom)) : ((new Nette\Utils\DateTime())->modify('- 1 year'));
+		$to = $statsTo ? (new Nette\Utils\DateTime($statsTo)) : (new Nette\Utils\DateTime());
 
 		$this->template->monthlyOrders = $this->orderRepository->getCustomerGroupedOrdersPrices($user, $from, $to, $currency);
 
-		$from = isset($this->statsFrom) ? (new Nette\Utils\DateTime($this->statsFrom)) : ((new Nette\Utils\DateTime())->modify('- 1 year'));
-		$to = isset($this->statsTo) ? (new Nette\Utils\DateTime($this->statsTo)) : (new Nette\Utils\DateTime());
+		$from = $statsFrom ? (new Nette\Utils\DateTime($statsFrom)) : ((new Nette\Utils\DateTime())->modify('- 1 year'));
+		$to = $statsTo ? (new Nette\Utils\DateTime($statsTo)) : (new Nette\Utils\DateTime());
 
 		$this->template->boughtCategories = $this->orderRepository->getCustomerOrdersCategoriesGroupedByAmountPercentage($user, $from, $to, $currency);
 
-		$from = isset($this->statsFrom) ? (new Nette\Utils\DateTime($this->statsFrom)) : ((new Nette\Utils\DateTime())->modify('- 1 year'));
-		$to = isset($this->statsTo) ? (new Nette\Utils\DateTime($this->statsTo)) : (new Nette\Utils\DateTime());
+		$from = $statsFrom ? (new Nette\Utils\DateTime($statsFrom)) : ((new Nette\Utils\DateTime())->modify('- 1 year'));
+		$to = $statsTo ? (new Nette\Utils\DateTime($statsTo)) : (new Nette\Utils\DateTime());
 
 		$this->template->topProducts = $this->orderRepository->getCustomerOrdersTopProductsByAmount($user, $from, $to, $currency);
 
@@ -116,9 +129,8 @@ class StatsControl extends Control
 
 	public function handleResetStatsFilter()
 	{
-		$this->statsFrom = null;
-		$this->statsTo = null;
-		$this->merchant = null;
+		$this->state = [];
+
 		$this->redirect('this');
 	}
 }
