8160bb28a932a88cbec1b187016ed2924b1baef8
Petr Křehlík <petr@lqd.cz>
2022-04-29 15:12:50 +0200

    fix color

--- src/Admin/Controls/ProductGridFactory.php
+++ src/Admin/Controls/ProductGridFactory.php
@@ -86,12 +86,12 @@ class ProductGridFactory
 			if ($object->hidden) {
 				$label = 'Neviditelný: Skrytý';
 				$color = 'danger';
-			} elseif ($object->unavailable) {
-				$label = 'Neviditelný: Neprodejný';
-				$color = 'danger';
 			} elseif ($object->getValue('priceCount') === '0') {
 				$label = 'Neviditelný: Bez ceny';
 				$color = 'danger';
+			} elseif ($object->unavailable) {
+				$label = 'Viditelný: Neprodejný';
+				$color = 'warning';
 			} elseif ($object->getValue('categoryCount') === '0') {
 				$label = 'Viditelný: Bez kategorie';
 				$color = 'warning';
