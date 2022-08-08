94904a6acbd808e46fa1f0a6a379cf641311095b
Oldrich <oldrich@lqd.cz>
2022-08-08 10:07:46 +0200

    payment log

---
+++ src/DB/PaymentLog.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eshop\DB;
+
+/**
+ * Log platba
+ * @table
+ */
+class PaymentLog extends \StORM\Entity
+{
+	/**
+	 * Vytvořeno
+	 * @column{"type":"datetime"}
+	 */
+	public string $created;
+	
+	/**
+	 * Výřešeno
+	 * @column{"type":"datetime"}
+	 */
+	public ?string $solved;
+	
+	/**
+	 * Externí ID
+	 * @column
+	 */
+	public ?string $externalId;
+	
+	/**
+	 * Externí code
+	 * @column
+	 */
+	public string $externalCode;
+	
+	/**
+	 * Částka
+	 * @column
+	 */
+	public float $amount;
+	
+	/**
+	 * Měna
+	 * @relation
+	 * @constraint
+	 */
+	public Currency $currency;
+	
+	/**
+	 * Typ platby
+	 * @constraint{"onUpdate":"CASCADE","onDelete":"CASCADE"}
+	 * @relation
+	 */
+	public ?PaymentType $type;
+	
+	/**
+	 * Spárovaná platba
+	 * @constraint{"onUpdate":"CASCADE","onDelete":"CASCADE"}
+	 * @relation
+	 */
+	public ?Payment $payment;
+}
