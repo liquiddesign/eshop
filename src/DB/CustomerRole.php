2d5dbe30ed7107ffe3e7ddfc49f18b6558afb680
Radek Picek <radek@lqd.cz>
2022-09-12 07:43:30 +0200

    rebase na main

---
+++ src/DB/CustomerRole.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eshop\DB;
+
+use StORM\RelationCollection;
+
+/**
+ * Role uzivatelů uživatelů
+ * @table
+ */
+class CustomerRole extends \StORM\Entity
+{
+	/**
+	 * Jméno
+	 * @column
+	 */
+	public string $name;
+
+	/**
+	 *
+	 * @column
+	 */
+	public int $priority;
+
+	/**
+	 * Systémová
+	 * @column
+	 */
+	public bool $systemic = false;
+
+	public function isSystemic(): bool
+	{
+		return $this->systemic;
+	}
+}
