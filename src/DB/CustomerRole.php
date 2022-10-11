5f3bbeafaddc25e7ba595e4d83e755a67454f75c
Radek Picek <radek@lqd.cz>
2022-10-11 15:42:22 +0200

    no message

---
+++ src/DB/CustomerRole.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eshop\DB;
+
+use Eshop\Common\DB\SystemicEntity;
+
+/**
+ * Role uzivatelů uživatelů
+ * @table
+ */
+class CustomerRole extends SystemicEntity
+{
+	/**
+	 * Jméno
+	 * @column
+	 */
+	public string $name;
+
+	/**
+	 * @column
+	 */
+	public int $priority;
+
+	/**
+	 * fixní provize v Kč
+	 * @column
+	 */
+	public float $provisionCzk;
+
+	/**
+	 * procentuální provize
+	 * @column
+	 */
+	public int $provisionPct;
+
+	/**
+	 * z první objednavky uživatele máme vetsi provizi
+	 * @column
+	 */
+	public int $firstProvisionPct;
+
+	/**
+	 * zda je role zapojena nejak do provizniho systemu
+	 * @column{"type":"enum","length":"'tree','direct'"}
+	 */
+	public string $affiliate = 'direct';
+
+	/**
+	 * procentuální sleva pro moje cleny
+	 * @column
+	 */
+	public int $membersDiscountPct;
+
+	/**
+	 * fixni sleva pro členy na první objednávku v Kč
+	 * @column
+	 */
+	public float $membersFirstOrderCzk;
+
+	/**
+	 * Procentuální sleva pro členy na první objednávku
+	 * @column
+	 */
+	public float $membersFirstOrderPct;
+
+	/**
+	 * sleva na produkty v %
+	 * @column
+	 */
+	public int $discount;
+
+	/**
+	 * Procentualni provize z opakovanych rays club objednavek
+	 * @column
+	 */
+	public float $raysClubRepeatProvisionPct;
+
+	/**
+	 * Umožnit výběr peněz
+	 * @column
+	 */
+	public bool $allowWithdraw;
+}
