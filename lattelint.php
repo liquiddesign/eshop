20072ca15961f167c647e844f85bc55986fa7abc
Oldrich <oldrich@lqd.cz>
2023-01-08 11:22:18 +0100

    latte 3.0 support

--- lattelint.php
+++ lattelint.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 $engine = new Latte\Engine;
+
 $engine->addExtension(new \Nette\Bridges\ApplicationLatte\UIExtension(null));
 $engine->addExtension(new \Nette\Bridges\FormsLatte\FormsExtension());
 $engine->addExtension(new Latte\Essential\TranslatorExtension(fn($val) => $val));
