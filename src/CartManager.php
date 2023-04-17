51e0f00899d9eabc6c7d4f37e9a1119a03b66b82
Petr Křehlík <pet@lqd.cz>
2023-04-17 09:51:03 +0200

    update

--- src/CartManager.php
+++ src/CartManager.php
@@ -3,14 +3,111 @@
 namespace Eshop;
 
 use Eshop\Common\CheckInvalidAmount;
+use Eshop\DB\Cart;
 use Eshop\DB\CartItem;
+use Eshop\DB\CartItemRepository;
+use Eshop\DB\CartItemTaxRepository;
 use Eshop\DB\Product;
+use Eshop\DB\TaxRepository;
 use Eshop\DB\Variant;
+use Nette\SmartObject;
+use Nette\Utils\Arrays;
+use StORM\Collection;
 
 class CartManager
 {
-	public function __construct(private readonly ShopperUser $shopperUser,)
-	{
+	use SmartObject;
+	public const DEFAULT_MIN_BUY_COUNT = 1;
+	public const DEFAULT_MAX_BUY_COUNT = 999999999;
+
+	/**
+	 * @var array<callable>&callable(\Eshop\DB\Customer): void ; Occurs after customer create
+	 */
+	public array $onCustomerCreate;
+
+	/**
+	 * @var array<callable>&callable(\Eshop\DB\Order): void ; Occurs after order create
+	 */
+	public array $onOrderCreate;
+
+	/**
+	 * @var array<callable> Occurs after cart create
+	 */
+	public array $onCartCreate = [];
+
+	/**
+	 * @var array<callable>&callable(): void ; Occurs after cart item delete
+	 */
+	public array $onCartItemDelete;
+
+	/**
+	 * @var array<callable(\Eshop\DB\CartItem): void>; Occurs after cart item create
+	 */
+	public array $onCartItemCreate = [];
+
+	/**
+	 * @var array<callable>&callable(): void ; Occurs after cart item updated
+	 */
+	public array $onCartItemUpdate;
+
+	/**
+	 * @var array<mixed>|null
+	 */
+	public ?array $orderCodeArguments = null;
+
+	protected Collection $paymentTypes;
+
+	protected ?float $sumPrice = null;
+
+	protected ?float $sumPriceVat = null;
+
+	protected ?float $sumPriceBefore = null;
+
+	protected ?float $sumPriceVatBefore = null;
+
+	protected ?int $sumAmount = null;
+
+	protected ?int $sumAmountTotal = null;
+
+	protected ?float $sumWeight = null;
+
+	protected ?float $sumDimension = null;
+
+	protected ?int $sumPoints = null;
+
+	protected ?float $maxWeight = null;
+
+	protected ?int $maxDimension = null;
+
+	protected bool $autoFixCart;
+
+	/**
+	 * @var bool|null Cached status of discount coupon validity during request
+	 */
+	protected ?bool $isDiscountCouponValid = null;
+
+	/**
+	 * @var array<string>
+	 */
+	protected array $checkoutSequence;
+
+	protected ?string $cartToken;
+
+	protected ?string $lastOrderToken;
+
+	/**
+	 * @var array<\Eshop\DB\Cart>|array<null>
+	 */
+	protected array $unattachedCarts = [];
+
+	protected int $cartExpiration = 30;
+
+	public function __construct(
+		private readonly ShopperUser $shopperUser,
+		private readonly TaxRepository $taxRepository,
+		private readonly CartItemRepository $cartItemRepository,
+		private readonly CartItemTaxRepository $cartItemTaxRepository,
+	) {
 	}
 
 	/**
@@ -42,7 +139,7 @@ class CartManager
 
 		$disabled = false;
 
-		if ($checkCanBuy !== false && !$this->canBuyProduct($product)) {
+		if ($checkCanBuy !== false && !$this->shopperUser->canBuyProduct($product)) {
 			if ($checkCanBuy === true) {
 				throw new BuyException('Product is not for sell', BuyException::NOT_FOR_SELL);
 			}
@@ -50,31 +147,27 @@ class CartManager
 			$disabled = true;
 		}
 
-		if (\is_null($checkInvalidAmount) || \is_bool($checkInvalidAmount)) {
-			if ($checkInvalidAmount !== false && !$this->checkAmount($product, $amount)) {
-				if ($checkInvalidAmount === true) {
-					throw new BuyException("Invalid amount: $amount", BuyException::INVALID_AMOUNT);
-				}
-
-				$disabled = true;
+		if (!$this->shopperUser->canBuyProductAmount($product, $amount)) {
+			if ($checkInvalidAmount === CheckInvalidAmount::CHECK_THROW) {
+				throw new BuyException("Invalid amount: $amount", BuyException::INVALID_AMOUNT);
 			}
-		} elseif (\is_int($checkInvalidAmount)) {
-			if ($checkInvalidAmount === CheckInvalidAmount::SET_DEFAULT_AMOUNT) {
-				$amount = $this->checkAmount($product, $amount) ? $amount : $product->defaultBuyCount;
+
+			if ($checkInvalidAmount === CheckInvalidAmount::CHECK_NO_THROW) {
+				$disabled = true;
+			} elseif ($checkInvalidAmount === CheckInvalidAmount::SET_DEFAULT_AMOUNT) {
+				$amount = $product->defaultBuyCount;
 			}
 		}
 
-		if ($replaceMode !== null && $item = $this->itemRepository->getItem($cart ?? $this->getCart(), $product, $variant)) {
-			$this->changeItemAmount($product, $variant, $replaceMode ? $amount : $item->amount + $amount, $checkInvalidAmount, $cart);
+		if ($replaceMode !== null && $item = $this->cartItemRepository->getItem($this->getCart(), $product, $variant)) {
+			$this->changeItemAmount($product, $variant, $replaceMode ? $amount : $item->amount + $amount, $checkInvalidAmount);
 
-			if ($this->onCartItemCreate) {
-				$this->onCartItemCreate($item);
-			}
+			Arrays::invoke($this->onCartItemCreate($item));
 
 			return $item;
 		}
 
-		$cartItem = $this->itemRepository->syncItem($cart ?? $this->getCart(), null, $product, $variant, $amount, $disabled);
+		$cartItem = $this->cartItemRepository->syncItem($this->getCart(), null, $product, $variant, $amount, $disabled);
 
 		if ($upsell) {
 			$cartItem->update(['upsell' => $upsell->getPK(),]);
@@ -95,15 +188,53 @@ class CartManager
 
 		$this->refreshSumProperties();
 
-		if ($this->onCartItemCreate) {
-			$this->onCartItemCreate($cartItem);
-		}
+		Arrays::invoke($this->onCartItemCreate($cartItem));
+
 
 		return $cartItem;
+	}
+
+	public function cartExists(): bool
+	{
+		if ($this->customer) {
+			if ($this->customer->activeCart) {
+				return true;
+			}
+
+			$this->customer->activeCart = null;
+
+			return false;
+		}
+
+		if (!\array_key_exists($this->cartToken, $this->unattachedCarts)) {
+			$this->unattachedCarts[$this->cartToken] = $this->cartRepository->getUnattachedCart($this->cartToken);
+		}
+
+		return (bool) $this->unattachedCarts[$this->cartToken];
+	}
+
+	public function getCart(): Cart
+	{
+		if (!$this->cartExists()) {
+			return $this->createCart();
+		}
+
+		return ($customer = $this->shopperUser->getCustomer()) ? $customer->activeCart : $this->unattachedCarts[$this->cartToken];
 	}
 
 	private function checkCurrency(Product $product): bool
 	{
 		return $product->getValue('currencyCode') === $this->getCart()->currency->code;
+	}
+
+	private function refreshSumProperties(): void
+	{
+		$this->sumPrice = null;
+		$this->sumPriceVat = null;
+		$this->sumAmountTotal = null;
+		$this->sumAmount = null;
+		$this->sumWeight = null;
+		$this->sumPoints = null;
+		$this->sumDimension = null;
 	}
 }
--- src/ShopperUser.php
+++ src/ShopperUser.php
@@ -3,17 +3,35 @@
 namespace Eshop;
 
 use Eshop\DB\Customer;
+use Eshop\DB\CustomerGroup;
+use Eshop\DB\CustomerGroupRepository;
 use Eshop\DB\Merchant;
+use Eshop\DB\Product;
+use Nette\Security\Authenticator;
+use Nette\Security\Authorizator;
+use Nette\Security\IAuthenticator;
+use Nette\Security\IUserStorage;
 use Nette\Security\User;
+use Nette\Security\UserStorage;
 
-class ShopperUser
+class ShopperUser extends User
 {
+	protected const MERCHANT_CATALOG_PERMISSIONS = 'price';
+
 	protected ?Customer $customer = null;
 
 	protected readonly CartManager $cartManager;
 
-	public function __construct(private readonly User $user, CartManagerFactory $cartManagerFactory,)
-	{
+	public function __construct(
+		CartManagerFactory $cartManagerFactory,
+		private readonly CustomerGroupRepository $customerGroupRepository,
+		?IUserStorage $legacyStorage = null,
+		?IAuthenticator $authenticator = null,
+		?Authorizator $authorizator = null,
+		?UserStorage $storage = null
+	) {
+		parent::__construct($legacyStorage, $authenticator, $authorizator, $storage);
+
 		$this->cartManager = $cartManagerFactory->create($this);
 	}
 
@@ -23,9 +41,9 @@ class ShopperUser
 			return $this->customer;
 		}
 
-		$identity = $this->user->getIdentity();
+		$identity = $this->getIdentity();
 
-		if ($this->user->isLoggedIn()) {
+		if ($this->isLoggedIn()) {
 			if ($identity instanceof Customer) {
 				return $identity;
 			}
@@ -40,5 +58,63 @@ class ShopperUser
 		}
 
 		return null;
+	}
+
+	public function canBuyProductAmount(Product $product, $amount): bool
+	{
+		return !($amount < $product->minBuyCount || ($product->maxBuyCount !== null && $amount > $product->maxBuyCount));
+	}
+
+	public function getMerchant(): ?Merchant
+	{
+		return $this->isLoggedIn() && $this->getIdentity() instanceof Merchant ? $this->getIdentity() : null;
+	}
+
+	public function canBuyProduct(Product $product): bool
+	{
+		return !$product->unavailable && $product->getValue('price') !== null && $this->getBuyPermission();
+	}
+
+	public function getBuyPermission(): bool
+	{
+		$customer = $this->getCustomer();
+		$merchant = $this->getMerchant();
+
+		if ($merchant && (!$customer && !$merchant->activeCustomerAccount)) {
+			return false;
+		}
+
+		if (!$customer || !$catalogPermission = $customer->getCatalogPermission()) {
+			return $this->getCustomerGroup() ? $this->getCustomerGroup()->defaultBuyAllowed : false;
+		}
+
+		return $catalogPermission->buyAllowed;
+	}
+
+	public function getCustomerGroup(): ?CustomerGroup
+	{
+		$customer = $this->getCustomer();
+
+		return $this->customerGroup ??= $customer ? $customer->group : $this->customerGroupRepository->getUnregisteredGroup();
+	}
+
+	public function getCatalogPermission(): string
+	{
+		$customer = $this->getCustomer();
+		$merchant = $this->getMerchant();
+
+		if ($merchant && (!$customer && !$merchant->activeCustomerAccount)) {
+			return self::MERCHANT_CATALOG_PERMISSIONS;
+		}
+
+		if (!$customer) {
+			return $this->getCustomerGroup()->defaultCatalogPermission;
+		}
+
+		if (!$catalogPermission = $customer->getCatalogPermission()) {
+			return 'none';
+		}
+
+		return $catalogPermission->catalogPermission;
 	}
 }
