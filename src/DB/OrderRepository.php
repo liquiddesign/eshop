33bf9ee6d828a246418452d9726108a715a0bb94
Petr Krehlik <73128826+petr6@users.noreply.github.com>
2023-10-02 13:21:49 +0200

    feat(admin): add method to recalculate prices multiple in grid

--- src/DB/OrderRepository.php
+++ src/DB/OrderRepository.php
@@ -9,6 +9,7 @@ use Admin\DB\IGeneralAjaxRepository;
 use Base\ShopsConfig;
 use Carbon\Carbon;
 use Common\DB\IGeneralRepository;
+use Eshop\Admin\HelperClasses\MultipleOperationResult;
 use Eshop\Admin\SettingsPresenter;
 use Eshop\Integration\Integrations;
 use Eshop\ShopperUser;
@@ -154,6 +155,31 @@ class OrderRepository extends \StORM\Repository implements IGeneralRepository, I
 	}
 
 	/**
+	 * @param \StORM\Collection<\Eshop\DB\Order> $orders
+	 * @return \Eshop\Admin\HelperClasses\MultipleOperationResult<\Eshop\DB\Order>
+	 */
+	public function recalculateOrderPricesMultiple(Collection $orders): MultipleOperationResult
+	{
+		$result = new MultipleOperationResult();
+
+		foreach ($orders as $order) {
+			try {
+				$this->recalculateOrderPrices($order);
+
+				$result->addCompleted($order);
+			} catch (\Exception $e) {
+				if ($e->getCode() === 1 || $e->getCode() === 2) {
+					$result->addIgnored($order);
+				} else {
+					$result->addFailed($order);
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	/**
 	 * Recalculate all CartItem in Order with new prices based on Customer in order
 	 * WARNING! Does not recalculate with related items!
 	 */
@@ -176,7 +202,7 @@ class OrderRepository extends \StORM\Repository implements IGeneralRepository, I
 		$discountCoupon = $order->getDiscountCoupon();
 
 		if (!$customer) {
-			throw new \Exception('Přepočet cen lze provádět jen pokud má objednávka přiřazeného registrovaného zákazníka!');
+			throw new \Exception('Přepočet cen lze provádět jen pokud má objednávka přiřazeného registrovaného zákazníka!', 1);
 		}
 
 		$customerGroup = $customer->group;
@@ -188,6 +214,8 @@ class OrderRepository extends \StORM\Repository implements IGeneralRepository, I
 		$this->shopsConfig->filterShopsInShopEntityCollection($visibilityLists);
 
 		$visibilityLists = $visibilityLists->where('this.hidden', false)->orderBy(['this.priority' => 'ASC'])->toArray();
+
+		$skipped = true;
 
 		foreach ($cartItems as $cartItem) {
 			$cartItemOld = clone $cartItem;
@@ -211,6 +239,8 @@ class OrderRepository extends \StORM\Repository implements IGeneralRepository, I
 					'price' => $product->getPrice(),
 					'priceVat' => $product->getPriceVat(),
 				]);
+
+				$skipped = false;
 			}
 
 			if (!$admin || !$isPriceChange) {
@@ -220,6 +250,10 @@ class OrderRepository extends \StORM\Repository implements IGeneralRepository, I
 			$priceChange = " | Cena z $cartItemOld->price $currencySymbol na $cartItem->price $currencySymbol";
 
 			$this->orderLogItemRepository->createLog($order, OrderLogItem::ITEM_EDITED, $cartItem->productName . $priceChange, $admin);
+		}
+
+		if ($skipped) {
+			throw new \Exception('Přeskočeno, žádné položky nevyžadují úpravu nebo některé položky nebylo možné upravit', 2);
 		}
 	}
 
