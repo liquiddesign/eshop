4476e8f184faa8930142ee810531c27db5b5a8cd
Petr Krehlik <73128826+petr6@users.noreply.github.com>
2023-11-09 14:30:43 +0100

    feat: add $formatDecimals to filterPrice

--- src/ShopperUser.php
+++ src/ShopperUser.php
@@ -712,12 +712,12 @@ class ShopperUser extends User
 
 	public function addFilters(\Nette\Bridges\ApplicationLatte\Template $template): void
 	{
-		$template->addFilter('price', function ($number, ?string $currencyCode = null) {
-			return $this->filterPrice($number, $currencyCode);
+		$template->addFilter('price', function ($number, ?string $currencyCode = null, ?int $formatDecimals = null) {
+			return $this->filterPrice($number, $currencyCode, $formatDecimals);
 		});
 
-		$template->addFilter('priceNoZero', function ($number, ?string $currencyCode = null) {
-			return $this->filterPriceNoZero($number, $currencyCode);
+		$template->addFilter('priceNoZero', function ($number, ?string $currencyCode = null, ?int $formatDecimals = null) {
+			return $this->filterPriceNoZero($number, $currencyCode, $formatDecimals);
 		});
 	}
 
@@ -726,7 +726,7 @@ class ShopperUser extends User
 	 * @param float|int $number
 	 * @param string|null $currencyCode
 	 */
-	public function filterPrice($number, ?string $currencyCode = null): string
+	public function filterPrice($number, ?string $currencyCode = null, ?int $formatDecimals = null): string
 	{
 		$currency = $this->getCurrency($currencyCode);
 
@@ -735,8 +735,10 @@ class ShopperUser extends User
 			$currency->formatDecimals = (int) ($localeInfo['frac_digits'] ?? 0);
 		}
 
+		$formatDecimals ??= $currency->formatDecimals;
+
 		$nbsp = \html_entity_decode('&nbsp;');
-		$formatted = \number_format((float) $number, $currency->formatDecimals, $currency->formatDecimalSeparator, \str_replace(' ', $nbsp, $currency->formatThousandsSeparator));
+		$formatted = \number_format((float) $number, $formatDecimals, $currency->formatDecimalSeparator, \str_replace(' ', $nbsp, $currency->formatThousandsSeparator));
 
 		return ($currency->formatSymbolPosition !== 'after' ? $currency->symbol : '') . $formatted . $nbsp . ($currency->formatSymbolPosition === 'after' ? $currency->symbol : '');
 	}
@@ -746,9 +748,9 @@ class ShopperUser extends User
 	 * @param float|int $number
 	 * @param string|null $currencyCode
 	 */
-	public function filterPriceNoZero($number, ?string $currencyCode = null): string
+	public function filterPriceNoZero($number, ?string $currencyCode = null, ?int $formatDecimals = null): string
 	{
-		return $number > 0 ? $this->filterPrice($number, $currencyCode) : '';
+		return $number > 0 ? $this->filterPrice($number, $currencyCode, $formatDecimals) : '';
 	}
 
 	/**
