4ab843ddfe7b8768a28cab062ea9ccef7715a887
Tomáš Pasičnyk <tomas@lqd.cz>
2024-04-03 09:25:13 +0200

    mc profi - buy form

--- src/Controls/BuyForm.php
+++ src/Controls/BuyForm.php
@@ -21,7 +21,7 @@ class BuyForm extends Form
 	public array $onItemAddedToCart = [];
 
 	public ?bool $replaceMode = false;
-
+	
 	public function __construct(Product $product, Shopper $shopper, CheckoutManager $checkoutManager)
 	{
 		parent::__construct();
@@ -29,18 +29,18 @@ class BuyForm extends Form
 		$defaultBuyCount = $product->defaultBuyCount;
 		$minCount = $product->minBuyCount ?? CheckoutManager::DEFAULT_MIN_BUY_COUNT;
 		$maxCount = $product->maxBuyCount ?? CheckoutManager::DEFAULT_MAX_BUY_COUNT;
-
+		
 		$countInput = $this->addInteger('amount', 'Počet zboží:')
 			->setDefaultValue($product->defaultBuyCount)
-			//			->setHtmlAttribute('step', $product->buyStep)
-			//			->addRule($this::MIN, 'Prosím zadejte minimální odběr %d ' . $product->unit, $product->minBuyCount)
-			->addRule([$this, 'validateNumber'], 'Prosím zadejte min. objednávku nebo její násobek: %d ' . $product->unit, [$product->buyStep, $minCount, $maxCount, $defaultBuyCount])
+			->setHtmlAttribute('step', $product->buyStep)
+			->addRule($this::MIN, 'Zadejte prosím min. objednávku: %d ' . $product->unit, $product->minBuyCount)
+			->addRule([$this, 'validateNumber'], 'Zadejte prosím násobek min. objednávky: %d ' . $product->unit, [$product->buyStep, $minCount, $maxCount, $defaultBuyCount])
 			->setRequired();
 		
 		if ($maxCount !== null) {
-			$countInput->addRule($this::MAX, null, $maxCount);
+			$countInput->addRule($this::MAX, 'Zadejte prosím maximální odběr %d ' . $product->unit, $maxCount);
 		}
-		
+
 		$this->addHidden('itemId', $product->getPK());
 		$this->addHidden('variant');
 		$this->addSubmit('submit', 'Přidat do košíku')->setDisabled(!$shopper->getBuyPermission());
