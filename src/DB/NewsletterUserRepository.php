9d898a1e7dc7e27749d4d914a5132ffd9dd5b857
Petr Křehlík <petrkrehlik@outlook.com>
2024-07-09 12:27:34 +0200

    feat: Add shop support to newsletter user operations

--- src/DB/NewsletterUserRepository.php
+++ src/DB/NewsletterUserRepository.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Eshop\DB;
 
+use Base\DB\Shop;
+use Base\ShopsConfig;
 use Security\DB\AccountRepository;
 use StORM\DIConnection;
 use StORM\SchemaManager;
@@ -15,14 +17,14 @@ class NewsletterUserRepository extends \StORM\Repository
 {
 	private AccountRepository $accountRepository;
 
-	public function __construct(DIConnection $connection, SchemaManager $schemaManager, AccountRepository $accountRepository)
+	public function __construct(DIConnection $connection, SchemaManager $schemaManager, AccountRepository $accountRepository, protected readonly ShopsConfig $shopsConfig)
 	{
 		parent::__construct($connection, $schemaManager);
 
 		$this->accountRepository = $accountRepository;
 	}
 
-	public function registerEmail(string $email): void
+	public function registerEmail(string $email, Shop|string|null $shop = null): void
 	{
 		$existingAccount = $this->accountRepository->many()
 			->join(['cm' => 'eshop_catalogpermission'], 'this.uuid = cm.fk_account', [], 'INNER')
@@ -35,16 +37,26 @@ class NewsletterUserRepository extends \StORM\Repository
 			$values['customerAccount'] = $existingAccount->getPK();
 		}
 
+		$values['shop'] = $shop instanceof Shop ? $shop->getPK() : (\is_string($shop) ? $shop : $this->shopsConfig->getSelectedShop());
+
 		$this->syncOne($values);
 	}
 
-	public function isEmailRegistered(string $email): bool
+	public function isEmailRegistered(string $email, Shop|string|null $shop = null): bool
 	{
-		return !$this->many()->where('this.email', $email)->isEmpty();
+		$query = $this->many()->where('this.email', $email);
+
+		$this->shopsConfig->filterShopsInShopEntityCollection($query, $shop);
+
+		return !$query->isEmpty();
 	}
 
-	public function unregisterEmail(string $email): void
+	public function unregisterEmail(string $email, Shop|string|null $shop = null): void
 	{
-		$this->many()->where('this.email', $email)->delete();
+		$query = $this->many()->where('this.email', $email);
+
+		$this->shopsConfig->filterShopsInShopEntityCollection($query, $shop);
+
+		$query->delete();
 	}
 }
