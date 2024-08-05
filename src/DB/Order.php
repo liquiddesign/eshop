d42d4758b55008c049ad61b0a897386113de0cf4
Petr Krehlik <73128826+petr6@users.noreply.github.com>
2024-08-05 13:22:25 +0200

    fix: Add getDeliveries() method to Order class and update usage

--- src/DB/Order.php
+++ src/DB/Order.php
@@ -11,6 +11,7 @@ use StORM\RelationCollection;
 /**
  * Objednávka
  * @method \StORM\RelationCollection<\Eshop\DB\Package> getPackages()
+ * @method \StORM\RelationCollection<\Eshop\DB\Delivery> getDeliveries()
  * @method \StORM\RelationCollection<\Eshop\DB\PaymentResult> getPaymentResults()
  * @table
  * @index{"name":"order_code","unique":true,"columns":["code", "fk_shop"]}
