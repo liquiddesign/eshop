7e2d3ae96c4dacf977c716aecec02165c7266b9d
Petr Křehlík <petrkrehlik@outlook.com>
2024-09-26 11:22:37 +0200

    feat: add PackageItem interface and implement in PackageItem and RelatedPackageItem

--- src/DB/RelatedPackageItem.php
+++ src/DB/RelatedPackageItem.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Eshop\DB;
 
+use Eshop\Common\DB\IPackageItem;
+
 /**
  * Item of package set item
  * @table
  * @index{"name":"related_package_item_unique","unique":true,"columns":["fk_packageItem","fk_cartItem"]}
  */
-class RelatedPackageItem extends \StORM\Entity
+class RelatedPackageItem extends \StORM\Entity implements IPackageItem
 {
 	/**
 	 * Main item
@@ -24,4 +26,14 @@ class RelatedPackageItem extends \StORM\Entity
 	 * @relation
 	 */
 	public RelatedCartItem $cartItem;
+
+	public function getProduct(): Product|null
+	{
+		return $this->cartItem->getProduct();
+	}
+
+	public function getAmount(): int
+	{
+		return $this->cartItem->amount;
+	}
 }
