1119fe3eaf70e64376267d488456ce61bc68a02b
petrk <petr@lqd.cz>
2025-02-24 19:57:37 +0100

    fix: Update children property type to mixed for better flexibility

--- src/DB/Category.php
+++ src/DB/Category.php
@@ -6,7 +6,6 @@ namespace Eshop\DB;
 
 use Eshop\Common\DB\SystemicEntity;
 use Nette\Utils\Strings;
-use StORM\ArrayWrapper;
 use StORM\Collection;
 use StORM\ICollection;
 use StORM\RelationCollection;
@@ -164,9 +163,9 @@ class Category extends SystemicEntity
 
 	/**
 	 * Pomocí repositářové metody getTree(array $orderBy)
-	 * @var array<\Eshop\DB\Category>|\StORM\ArrayWrapper<\Eshop\DB\Category>
+	 * @var array<<\Eshop\DB\Category>>|\StORM\ArrayWrapper<\Eshop\DB\Category>
 	 */
-	public array|ArrayWrapper $children = [];
+	public mixed $children;
 
 	/**
 	 * Nadřazený
