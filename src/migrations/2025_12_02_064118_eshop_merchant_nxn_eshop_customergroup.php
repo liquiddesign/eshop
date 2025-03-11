cf432701238ff214351a96a454e74a1a04687812
petrk <petr@lqd.cz>
2025-03-11 11:36:57 +0100

    fix: Remove unused migration code for eshop_merchant_nxn_eshop_customergroup

--- src/migrations/2025_12_02_064118_eshop_merchant_nxn_eshop_customergroup.php
+++ src/migrations/2025_12_02_064118_eshop_merchant_nxn_eshop_customergroup.php
@@ -8,37 +8,14 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
 	public function up(): void
 	{
-		Schema::create('eshop_merchant_nxn_eshop_customergroup', function (Blueprint $table): void {
-			$table->foreignUuid('fk_merchant')->references('uuid')->on('eshop_merchant')->onDelete('cascade')->onUpdate('cascade');
-			$table->foreignUuid('fk_customerGroup')->references('uuid')->on('eshop_customergroup')->onDelete('cascade')->onUpdate('cascade');
-		});
-
 		DB::table('eshop_merchant_nxn_eshop_customergroup')->insertUsing(
 			['fk_merchant', 'fk_customerGroup'],
-			DB::table('eshop_merchant')->select('uuid', 'fk_customerGroup')
+			DB::table('eshop_merchant')->select('uuid', 'fk_customerGroup')->whereNotNull('fk_customerGroup')
 		);
 
 		Schema::table('eshop_merchant', function (Blueprint $table): void {
 			$table->dropForeign('eshop_merchant_customerGroup');
 			$table->dropColumn('fk_customerGroup');
 		});
-	}
-
-	public function down(): void
-	{
-		Schema::table('eshop_merchant', function (Blueprint $table): void {
-			$table->foreignUuid('fk_customerGroup')->nullable();
-		});
-
-		DB::table('eshop_merchant')->updateOrInsert(
-			['uuid' => DB::raw('eshop_merchant_nxn_eshop_customergroup.fk_merchant')],
-			['fk_customerGroup' => DB::raw('eshop_merchant_nxn_eshop_customergroup.fk_customerGroup')]
-		);
-
-		Schema::table('eshop_merchant', function (Blueprint $table): void {
-			$table->foreign('fk_customerGroup')->references('uuid')->on('eshop_customergroup')->onDelete('cascade')->onUpdate('cascade');
-		});
-
-		Schema::dropIfExists('eshop_merchant_nxn_eshop_customergroup');
 	}
 };
