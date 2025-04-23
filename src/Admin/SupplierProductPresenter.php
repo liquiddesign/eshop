04e1fbdd087ee0323867f91e23322b41c4336275
Hanes <jan@lqd.cz>
2025-04-23 16:10:44 +0800

    feat: Supplier product before update

--- src/Admin/SupplierProductPresenter.php
+++ src/Admin/SupplierProductPresenter.php
@@ -351,9 +351,14 @@ class SupplierProductPresenter extends BackendPresenter
 		$form->onSuccess[] = function (AdminForm $form) use ($supplierProduct): void {
 			$values = $form->getValues('array');
 
+			$this->connection->getLink()->beginTransaction();
+
 			try {
 				$supplierProduct->update($values);
+				$this->supplierProductFormBeforeRedirect($form, $supplierProduct);
+				$this->connection->getLink()->commit();
 			} catch (\Throwable $e) {
+				$this->connection->getLink()->rollBack();
 				if ((int) $e->getCode() === 23000 && Strings::contains($e->getMessage(), 'supplier_product_ean') !== false) {
 					$this->flashMessage('Duplicitní EAN!', 'error');
 
@@ -366,6 +371,10 @@ class SupplierProductPresenter extends BackendPresenter
 		};
 
 		return $form;
+	}
+
+	protected function supplierProductFormBeforeRedirect(AdminForm $adminForm, SupplierProduct $supplierProduct): void
+	{
 	}
 
 	public function renderPairAlgoliaBulk(array $ids): void
