89e7323e09f7556535c27a115293640abe08bb1d
petrk <petr@lqd.cz>
2025-09-23 08:22:54 +0200

    build: docker cli fix

--- docker/it.sh
+++ docker/it.sh
@@ -2,6 +2,7 @@ if [ -f .env ]; then
   . .env
 fi
 
-docker compose exec cli bash -c "chown ${APP_USER-www-data}:${APP_USER-www-data} -R /home/${APP_USER-www-data}"
-docker compose exec --user "${APP_USER-www-data}" cli bash -c "echo \"alias c='composer'\" >> $HOME/.bashrc"
+docker compose exec cli bash -c "chmod 777 -R /.composer"
+docker compose exec cli bash -c "chown -R ${APP_USER-www-data}:${APP_USER-www-data} ~${APP_USER-www-data}"
+docker compose exec --user "${APP_USER-www-data}" cli bash -c "echo \"alias c='composer'\" >> ~${APP_USER-www-data}/.bashrc"
 docker compose exec -it --user "${APP_USER-www-data}" cli bash
