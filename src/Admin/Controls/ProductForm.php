994c6bdec055a6409535c0ae707429764885dc91
Hanes <jan@lqd.cz>
2026-01-15 11:18:49 +0100

    chore: Disabling productStockCostPrice input

--- src/Admin/Controls/ProductForm.php
+++ src/Admin/Controls/ProductForm.php
@@ -254,12 +254,9 @@ Ostatní: Přebírání ze zvoleného zdroje
 			->setHtmlAttribute('data-info', 'Nákupní cena pro výpočet marže v nabídce, pokud neexistuje dodavatelský produkt.');
 		$manualPurchasePrice->addCondition($form::Filled)->addRule($form::Float);
 
-		$productStockCostPrice = $form->addText('productStockCostPrice', 'Skladová cena (ComparativePriceInclVAT)')
-			->setNullable()
-			->setHtmlType('number')
-			->setHtmlAttribute('step', 'any')
+		$form->addText('productStockCostPrice', 'Skladová cena (ComparativePriceInclVAT)')
+			->setDisabled()
 			->setHtmlAttribute('data-info', 'Manuálně vyplněná skladová cena pro výpočet maržových zarážek v nabídkách.');
-		$productStockCostPrice->addCondition($form::Filled)->addRule($form::Float);
 
 		$form->addLocaleText('unit', 'Jednotka');
 		//	->setHtmlAttribute('data-info', 'Např.: ks, ml, ...');
