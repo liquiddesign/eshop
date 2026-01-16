4a914676c7086cf2e97d4395c943f85de508bace
Hanes <jan@lqd.cz>
2026-01-16 12:25:47 +0100

    chore: chore

--- src/Admin/Controls/ProductForm.php
+++ src/Admin/Controls/ProductForm.php
@@ -254,7 +254,7 @@ Ostatní: Přebírání ze zvoleného zdroje
 			->setHtmlAttribute('data-info', 'Nákupní cena pro výpočet marže v nabídce, pokud neexistuje dodavatelský produkt.');
 		$manualPurchasePrice->addCondition($form::Filled)->addRule($form::Float);
 
-		$form->addText('productStockCostPrice', 'Skladová cena (ComparativePriceInclVAT)')
+		$form->addText('productStockCostPrice', 'Skladová cena (ProductStockCostPrice)')
 			->setDisabled()
 			->setHtmlAttribute('data-info', 'Manuálně vyplněná skladová cena pro výpočet maržových zarážek v nabídkách.');
 
