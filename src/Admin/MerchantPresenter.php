75eecf3965329b35c7d0060e6c072e1230a9fb12
Hanes <jan@lqd.cz>
2026-03-27 08:58:46 +0100

    feat(shared): Mirov string

--- src/Admin/MerchantPresenter.php
+++ src/Admin/MerchantPresenter.php
@@ -246,7 +246,7 @@ class MerchantPresenter extends BackendPresenter
 				'Základní: vidí nejnižší a průměrnou nákupní cenu. Plný: vidí i ceny jednotlivých dodavatelů.',
 			);
 			$form->addCheckbox('approveOfferPermission', 'Oprávnění: Schvalování nabídek');
-			$form->addCheckbox('qiProductPairingPermission', 'Oprávnění: Párování produktů QI');
+			$form->addCheckbox('qiProductPairingPermission', 'Oprávněni: Párování produktů + nastavení centrál');
 			$form->addCheckbox(
 				'customerEmailNotification',
 				'Posílat e-mailem informace o objednávkách přiřazených zákazníků.',
